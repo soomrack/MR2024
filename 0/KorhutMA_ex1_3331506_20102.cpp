1fba47c6528c534a8149228a883149426c0ba324
KorhutMA <korhut.peng@mail.ru>
2024-09-07 14:47:15 +0300

    KorhutMA_ex1_3331506_20102.cpp
    исправленный вариант

--- 0/KorhutMA_ex1_3331506_20102.cpp
+++ 0/KorhutMA_ex1_3331506_20102.cpp
@@ -4,9 +4,9 @@ int main()
 {
     setlocale(LC_ALL, "RU");
 
-    float a = 0; 
+    float a = 0;
     float b = 1000000;
-    float s = 250000; //зп изменил, тк иначе у Alice кончаются деньги в первый же год 
+    float s = 220000; //зп изменил, тк иначе у Alice кончаются деньги в первый же год 
     int h = 13000000; //квартира
     float i1 = 0.09; //инфляция
     float i2 = 0.07; //индексация
@@ -20,31 +20,33 @@ int main()
     float r = 30000; //аренда
 
     for (n; n > 0; n--) {
-        a = a + s; //ЗП
-        b = b + s;
-        a = a - (g+e+k+c); //расходы
-        b = b - (r+e+k+c);
+        for (int m = 12; m > 0; m--) {
+            a = a + s; //ЗП
+            b = b + s;
+            a = a - (g + e + k + c); //расходы за 12 мес.
+            b = b - (r + e + k + c);
+        }
         b = b * (1 + d); //депозит
         a = a * (1 + d); //Alice тоже кладет в банк оставшиеся деньги?
-        g = g * (1+i1); //инфляция
-        a = a * (1+i1); 
-        b = b * (1+i1);
+        g = g * (1 + i1); //инфляция
+        a = a * (1 + i1);
+        b = b * (1 + i1);
         e = e * (1 + i1);
         k = k * (1 + i1);
         c = c * (1 + i1);
         r = r * (1 + i1);
         h = h * (1 + i1);
         s = s * (1 + i2);
-        //std::cout << "\nAlice: " << a <<" dom: "<< h << "\nBob: " << b << "\n " << s; //проверка
+        std::cout << "\nAlice: " << a << " dom: " << h << "\nBob: " << b << "\n " << s; //проверка
     }
 
-    std::cout << "\nAlice: " << a+h << "\nBob: " << b;
-    if (a+h > b)
+    std::cout << "\nAlice: " << a + h << "\nBob: " << b;
+    if (a + h > b)
         std::cout << "\nИпотечное рабство лучше съемного";
-    else if (a+h == b)
+    else if (a + h == b)
         std::cout << "\nРабство-плохо";
-    else 
+    else
         std::cout << "\nСъемное рабство выгоднее ипотечного";
-   
+
     return 0;
 }
