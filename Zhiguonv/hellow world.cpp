60ab7f57eba476601525e0f41fd562071b1e5b0e
Zhigunov-Vasiliy <zhigunov.vasy@mail.ru>
2024-09-09 22:58:37 +0300

    Delete Zhiguonv directory

--- Zhiguonv/hellow world.cpp
+++
@@ -1,9 +0,0 @@
-﻿// hellow world.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <stdio.h>
-int main()
-{
-    printf("Hello World! \n");
-}
-
