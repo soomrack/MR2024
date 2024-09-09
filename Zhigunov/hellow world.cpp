99ffda940daa8071cf120167608a12c9450501c8
Zhigunov-Vasiliy <zhigunov.vasy@mail.ru>
2024-09-09 22:59:57 +0300

    Add files via upload

---
+++ Zhigunov/hellow world.cpp
@@ -0,0 +1,9 @@
+﻿// hellow world.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+int main()
+{
+    printf("Hello World! \n");
+}
+
