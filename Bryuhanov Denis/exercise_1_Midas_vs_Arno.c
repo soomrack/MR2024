659fe885a0887a082463593f0d0ff522b3e4359a
creper89 <creper_guy89@mail.ru>
2024-09-10 13:30:28 +0900

    Создание  типа Герой Инициализация Мидаса и Арно

--- Bryuhanov Denis/exercise_1_Midas_vs_Arno.c
+++ Bryuhanov Denis/exercise_1_Midas_vs_Arno.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
 
+
+typedef struct Hero
+{
+	int balance;
+	int salary;
+	int borrow;
+	int ipoteck_payment;
+	int month_payment;
+};
+
+
+//Hero midas;
+//Hero arno;
+
+/*
+void midas_init()
+{
+	midas.balance = 1 * 1000 * 1000;
+	midas.salary = 400 * 1000;
+	midas.borrow = 12 * 1000 * 1000;
+	midas.ipoteck_payment = 200 * 1000;
+	midas.month_payment = 100 * 1000;
+}
+
+
+void arno_init()
+{
+	arno.balance = 1 * 1000 * 1000;
+	arno.salary = 350 * 1000;
+	arno.borrow = 0;
+	arno.ipoteck_payment = 0;
+	arno.month_payment = (60 + 30) * 1000;
+}
+*/
+
+int main()
+{
+	//printf("%d", midas.balance);
+	//printf("%d", arno.borrow);
+
+	return 1;
+}
