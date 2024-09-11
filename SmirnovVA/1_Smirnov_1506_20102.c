219be79663be41d2925822773153d14213d0881a
Loughum <80972720+Loughum@users.noreply.github.com>
2024-09-11 13:21:47 +0300

    Add files via upload

---
+++ SmirnovVA/1_Smirnov_1506_20102.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    float salB, salA = 200000;
+    float  rentB = 30000; 
+    float mortA
+    float mortperc
+    int time, inf, ind;
+    printf("Hello, world!");
+    for (int i =0; i<t; i++)
+    {
+        int n; //мсавмвапвап
+        if (n/2020>=n%2020)
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+}
