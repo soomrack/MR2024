a8603b4bd5af4ccc2aefb5205fa62bae28f81b4e
FACE8D <136920080+FACE8D@users.noreply.github.com>
2024-09-17 14:11:07 +0300

    Fix all problems

--- VcherashnijZD/task1.c
+++ VcherashnijZD/task1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 
 
@@ -13,99 +13,91 @@ typedef struct
     Money cost;
 } Expenditure;
 
+typedef struct {
+    Money estate_cost;
+    Money cap;
+    Money remaining_month;
+    Percent rate;
+} Mortgage; // mortgage
+
+typedef struct {
+    Money money;
+    Percent rate;
+} Deposit;
+
 typedef struct
 {
     Money money;
     Money salary;
-    int vacation_month;
-
-    Money estate_cost;
-    Money deposit;
-    Percent deposit_rate;
+    uint16_t vacation_month; // bit field vacation, no payment
+    Mortgage mortgage;
+    Deposit deposit;
     Percent inflation;
- 
-    Money mortage_cap;
-    Money mortage_remaining_month;
-    Percent mortage_rate;
-
     Money rent;
-
-    Expenditure* consumers;
+    Expenditure consumers[4];
 } Person;
-
-const Expenditure basic_consumer[] = {
-    {"Food", 15*1000},
-    {"Service", 20*1000},
-    {"Personal", 20*1000},
-    {"End", 0}
-};
-#define EXPED_SIZE sizeof(basic_consumer)
-
-/*
- * Where is no need here for this function, OS will always free memory after proccess termination (if you are not in DOS). But for convenience.
- */
-void deinit(Person* person)
-{
-    free(person->consumers);
-}
 
 void bob_init(Person* person)
 {
-    person->vacation_month = -1;
+    person->vacation_month = 0;
     person->money = 0;
     person->salary = 300000;
 
-    person->estate_cost = 0;
-    person->deposit = 1000000;
+    person->deposit.money = 1000000;
+    person->deposit.rate = 0.1;
 
-    person->mortage_cap = 0;
-    person->mortage_remaining_month = 0;
-    person->mortage_rate = 0;
     person->rent = 30000;
-    person->deposit_rate = 0.1;
-
 
     person->inflation=0.07;
-    person->consumers = malloc(EXPED_SIZE);
-    memcpy(person->consumers, &basic_consumer, EXPED_SIZE);
+    const Expenditure consumers[] = {
+        {"Food", 15*1000},
+        {"Service", 20*1000},
+        {"Personal", 20*1000},
+        {"End", 0}
+    };
+    memcpy(person->consumers, consumers, sizeof(person->consumers));
 }
 
 void alice_init(Person* person, const int duration)
 {
     person->vacation_month = 8;
     person->money = 0;
-    person->salary = 300000;
+    person->salary = 500000;
 
-    person->estate_cost = 15000000;
-    person->deposit = 1000000;
+    person->deposit.money = 1000000;
+    person->deposit.rate = 0.1;
 
-    person->mortage_cap = person->estate_cost - person->deposit;
-    person->mortage_remaining_month = 12*duration;
-    person->mortage_rate = 0.24;
-    person->deposit_rate = 0.1;
-
-    person->rent = 0;
+    person->mortgage.estate_cost = 15000000;
+    person->mortgage.cap = person->mortgage.estate_cost - person->deposit.money;
+    person->deposit.money = 0;
+    person->mortgage.remaining_month = 12*duration;
+    person->mortgage.rate = 0.24;
 
     person->inflation=0.07;
-    person->consumers = malloc(EXPED_SIZE);
-    memcpy(person->consumers, &basic_consumer, EXPED_SIZE);
+    const Expenditure consumers[] = {
+        {"Food", 15*1000},
+        {"Service", 20*1000},
+        {"Personal", 10*1000},
+        {"End", 0}
+    };
+    memcpy(person->consumers, consumers, sizeof(person->consumers));
 }
 
 void salary(Person* person, int month)
 {
-    if (month != person->vacation_month)
+    if (!((person->vacation_month) & (1<<(month-1))))
         person->money += person->salary;
 }
 
-void mortage(Person* person)
+void mortgage(Person* person)
 {
-    if(person->mortage_cap != 0)
+    if(person->mortgage.cap != 0)
     {
-        person->money -= (Money)((Percent)person->mortage_cap * person->mortage_rate / 12.0); // mortage percentage payment
-        Money mortage_payment = person->mortage_cap / person->mortage_remaining_month;
-        person->money -= mortage_payment;
-        person->mortage_cap -= mortage_payment;
-        person->mortage_remaining_month--;
+        person->money -= (Money)((Percent)person->mortgage.cap * person->mortgage.rate / 12.0); // mortgage percentage payment
+        Money mortgage_payment = person->mortgage.cap / person->mortgage.remaining_month;
+        person->money -= mortgage_payment;
+        person->mortgage.cap -= mortgage_payment;
+        person->mortgage.remaining_month--;
     }
 }
 
@@ -126,19 +118,19 @@ void consumers(Person* person)
 
 void add_deposit(Person* person)
 {
-    person->deposit += person->money;
+    if (person->money < 0) return;
+    person->deposit.money += person->money;
     person->money = 0;
 }
 
 void deposit_growth(Person* person)
 {
-    person->deposit += (Money)((Percent)person->deposit * person->deposit_rate / 12.0);
+    person->deposit.money += (Money)((Percent)person->deposit.money * person->deposit.rate / 12.0D);
 }
 
 void index_salary(Person* person)
-{
-    
-    person->salary += (Money)((Percent)person->salary * person->inflation / 12.0);
+{    
+    person->salary += (Money)((Percent)person->salary * person->inflation / 12.0D);
 }
 
 void consumer_inflate(Person* person)
@@ -158,58 +150,67 @@ void rent_inflate(Person* person)
 
 void estate_inflate(Person* person)
 {
-    person->estate_cost += (Money)((Percent)person->estate_cost * person->inflation / 12.0);
+    person->mortgage.estate_cost += (Money)((Percent)person->mortgage.estate_cost * person->inflation / 12.0);
 }
 
-void month_cycle(Person* person, int month)
+void head_loop(Person* alice, Person* bob, const int current_year, const int current_month, const int duration)
 {
-    //person operations with money
-    salary(person, month);
-    mortage(person);
-    rent(person);
-    consumers(person);
-    add_deposit(person);
-
-    //processes
-    deposit_growth(person);
-    index_salary(person);
-    consumer_inflate(person);
-    rent_inflate(person);
-    estate_inflate(person);
-}
-
-void compare(Person* alice, Person* bob)
-{
-    if ((alice->estate_cost + alice->deposit) == bob->deposit)
-        printf("Bob and Alice funds are equal: %lu\n", bob->deposit);
-    else if (alice->estate_cost + alice->deposit > bob->deposit)
-        printf("Alice funds: %lu are higher than Bob's %lu\n", alice->estate_cost + alice->deposit, bob->deposit);
-    else
-        printf("Alice funds: %lu are lower than Bob's %lu\n", alice->estate_cost + alice->deposit, bob->deposit);
-}
-
-int main()
-{
-    const int duration = 25, current_year = 2024, month_start = 9;
-    Person alice, bob;
-    alice_init(&alice, duration);
-    bob_init(&bob);
-    uint16_t year = current_year;
-    int month = month_start;
-    while(year < current_year + duration || month < month_start)
+    int year = current_year;
+    int month = current_month;
+    while(year < current_year + duration || month < current_month)
     {
         if(++month > 12)
         {
             month = 1;
             year++;
         }
+        // person operations with money
+        // Alice lost job after five years for one month (task on lection). And get new job x1.5 salary
+        if (year == (current_year+5))
+            alice->salary = (Money)((Percent)alice->salary*2.0F);
+        else
+            salary(alice, month);
+        // Bob get promotion on job 10 years after
+        if (year == (current_year+10))
+            bob->salary = (Money)((Percent)bob->salary*4.0F);
+        salary(bob, month);
+        mortgage(alice);
+        rent(bob);
+        consumers(alice);
+        consumers(bob);
+        add_deposit(alice);
+        add_deposit(bob);
 
-        month_cycle(&alice, month);
-        month_cycle(&bob, month);
+        // processes
+        deposit_growth(alice);
+        deposit_growth(bob);
+        index_salary(alice);
+        index_salary(bob);
+        consumer_inflate(alice);
+        consumer_inflate(bob);
+        rent_inflate(bob);
+        estate_inflate(alice);
     }
-    deinit(&alice);
-    deinit(&bob);
-    compare(&alice, &bob);
+}
+
+void print_comparation(Person* alice, Person* bob)
+{
+    if ((alice->mortgage.estate_cost + alice->deposit.money) == bob->deposit.money)
+        printf("Bob and Alice funds are equal: %lu\n", bob->deposit.money);
+    else if (((Money) (alice->mortgage.estate_cost + alice->deposit.money)) > bob->deposit.money)
+        printf("Alice funds: %lu are higher than Bob's %lu\n", alice->mortgage.estate_cost + alice->deposit.money, bob->deposit.money);
+    else
+        printf("Alice funds: %lu are lower than Bob's %lu\n", alice->mortgage.estate_cost + alice->deposit.money, bob->deposit.money);
+}
+
+int main()
+{
+    const int duration = 25;
+    Person alice, bob;
+    alice_init(&alice, duration);
+    bob_init(&bob);
+    head_loop(&alice, &bob, 2024, 9, duration); // current year, month and duration
+    print_comparation(&alice, &bob);
 
     return 0;
 }
