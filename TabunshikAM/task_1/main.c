bf9e434f9a7513ff92b38b6476d692df6d141ceb
Табунщик Артём Михайлович <109157898+FOXFIREFOR@users.noreply.github.com>
2024-09-18 22:55:21 +0300

    Create main.c

---
+++ TabunshikAM/task_1/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+
+typedef long long int money; // в копейках
+
+const int inflation_p = 9; // процент инфляции
+
+typedef struct person
+{
+    money bank_account;
+    money earns;
+    money rent;
+    money mortgage;
+    money month_expences;
+    money 
+    money
+    money
+    money
+    money
+    money
+    money
+} person;
+
+person alice;
+person bob;
