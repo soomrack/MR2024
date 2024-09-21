4e8c357c72ca8f7d284ff5b42cd3a547aa6f575b
Kizaru855 <freeman1698@gmail.com>
2024-09-21 14:35:54 +0300

    Update ex1.c

--- TsypinNS/ex1.c
+++ TsypinNS/ex1.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-typedef long long int Money;
+typedef long long int Money; //RUB
 
 typedef struct {
     Money account;
     Money salary;
     Money food;
-    Money deposit;
     Money home_bills;
+    Money mortage;
+    Money rent;
 } Person;
 
 Person alice;
+Person bob;
 
 void alice_init()
 {
@@ -18,9 +20,19 @@ void alice_init()
     alice.salary = 200 * 1000 * 100;
     alice.food = 25 * 1000 * 100;
     alice.home_bills = 13 * 1000 * 1000 * 100 * 0.001;
+    alice.mortage = 174818 * 100 + 41;    //calcus.ru, ипотека на 13 миллионов под 16% без первоначального взноса
 }
 
-void alice_salary(const int month, const int year)
+void bob_init()
+{
+    bob.account = 0;
+    bob.salary = 200 * 1000 * 100;
+    bob.food = 25 * 1000 * 100;
+    bob.home_bills = 13 * 1000 * 1000 * 100 * 0.001;
+    bob.rent = 30 * 1000 * 100;
+}
+
+void alice_salary(const int month)
 {
     alice.account += alice.salary;
 
@@ -29,7 +41,16 @@ void alice_salary(const int month, const int year)
     }
 }
 
-void alice_food(const int month, const int year)
+void bob_salary(const int month)
+{
+    bob.account += bob.salary;
+
+    if(month == 12) {
+        bob.salary *= 1.07;
+    }
+}
+
+void alice_food(const int month)
 {
     alice.account -= alice.food;
 
@@ -38,17 +59,62 @@ void alice_food(const int month, const int year)
     }
 }
 
+void bob_food(const int month)
+{
+    bob.account -= bob.food;
+
+    if (month == 12) {
+        bob.food *= 1.08;
+    }
+}
+
 void alice_home_bills(const int month)
 {
     if(month == 12) {
         alice.account -= alice.home_bills;
-        alice.home_bills *= 1.07;
+        alice.home_bills *= 1.08;
     }
+}
+
+void bob_home_bills(const int month)
+{
+    if(month == 12) {
+        bob.account -= bob.home_bills;
+        bob.home_bills *= 1.08;
+    }
+}
+
+void alice_mortage_payment()
+{
+    alice.account -= alice.mortage;
+}
+
+void bob_rent(const int month)
+{
+    bob.account -= bob.rent;
+    if(month ==12){
+        bob.rent *= 1.08;
+    }
+}
+
+void alice_deposit()
+{
+    alice.account *= (1 + 0.2/12);
+}
+
+void bob_deposit()
+{
+    bob.account *= (1 + 0.2/12);
 }
 
 void alice_print()
 {
     printf("Alice account = %lld RUB\n", alice.account);
+}
+
+void bob_print()
+{
+    printf("Bob account = %lld RUB\n", bob.account);
 }
 
 void simulation()
@@ -58,11 +124,17 @@ void simulation()
 
     while( !((year == 2024 + 30) && (month == 10)) ){
 
-        alice_salary(month, year);
-        //alice_deposit_pp();
-        //alice_mortage_payment();
-        alice_food(month, year);
+        alice_salary(month);
+        alice_deposit();
+        alice_mortage_payment();
+        alice_food(month);
         alice_home_bills(month);
+
+        bob_salary(month);
+        bob_deposit();
+        bob_rent(month);
+        bob_food(month);
+        bob_home_bills(month);
 
         ++month;
         if(month==13) {
@@ -75,8 +147,19 @@ void simulation()
 int main()
 {
     alice_init();
+    bob_init();
     simulation();
     alice_print();
+    bob_print();
+    if (alice.account > bob.account){
+        printf("Alice won\n");
+    }
+    else if (bob.account > alice.account){
+        printf("Bob won\n");
+    }
+    else {
+        printf("Draw\n");
+    }
     return 0;
 
 }
