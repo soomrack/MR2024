5fad90d514c5007484801a208c9f8fae0dbb3be2
o1yx <sharyginn.09@gmail.com>
2024-09-22 14:37:56 +0300

    Add files via upload

--- SharyginAV/task1.c
+++ SharyginAV/task1.c
@@ -1,9 +1,16 @@
+/*
+дописать структуру deposit
+написать функцию депозита
+*/
+
 #include <stdio.h>
+#include <math.h>
 
 const int YEAR_START = 2024;
 const int MONTH_START = 9;
-const int DURATION = 1;
-const int inflation = 8;  // percent
+const int DURATION = 30;
+
+const double INFLATION = 8;  // percent
 
 typedef long long int Money;
 
@@ -13,23 +20,74 @@ struct person{
     Money salary;
     Money monthly_expenses;
     Money rent;
-} bob;
+} bob, alice;
 
 
 struct mortgage{
-    Money start_installment;
-    Money mortgage_payment;
-    int interest_rate;
+    Money first_installment;
+    Money apartment_cost;
+    Money monthly_payment;
+    double interest_rate, total_rate, monthly_rate;
+} mortgage;
+
+
+struct deposit{
     int duration;
-};
+    double rate;
+    Money capital, monthly_payment;
+} bob_deposit;
+
+
+void mortgage_payment(){
+    mortgage.first_installment = 1*1000*1000;
+    mortgage.apartment_cost = 15*1000*1000;
+    mortgage.interest_rate = 18;
+
+    mortgage.monthly_rate = mortgage.interest_rate / 12 / 100;
+    mortgage.total_rate = pow(1 + mortgage.monthly_rate, DURATION * 12);
+    mortgage.monthly_payment = (mortgage.apartment_cost - mortgage.first_installment) * mortgage.monthly_rate * mortgage.total_rate / (mortgage.total_rate - 1);
+
+}
+
+
+void inflation(month)
+{
+    if (month == 12)
+    {
+        mortgage.apartment_cost *= 1 + INFLATION / 100;
+        bob.rent *= 1 + INFLATION / 100;
+        bob.monthly_expenses *= 1 + INFLATION / 100;
+        alice.monthly_expenses *= 1 + INFLATION / 100;
+
+    }
+}
+
+
+void deposit()
+{   
+    bob_deposit.rate = 17.0;
+    bob_deposit.capital += bob.bank_account;
+    bob.bank_account = 0;  // весь остаток положил на вклад
+    bob_deposit.monthly_payment = bob_deposit.capital * bob_deposit.rate / 100. / 12.;
+    bob.bank_account += bob_deposit.monthly_payment;
+}
 
 
 void bob_init()
 {
-    bob.bank_account = 0;
+    bob.bank_account = 1*1000*1000;
     bob.salary = 300 * 1000;
     bob.monthly_expenses = 40 * 1000;
     bob.rent = 30 * 1000;
+}
+
+
+void alice_init()
+{
+    alice.bank_account = 0;
+    alice.salary = 300 * 1000;
+    alice.monthly_expenses = 40 * 1000;
+    alice.rent = 0;
 }
 
 
@@ -38,7 +96,17 @@ void bob_salary(int month)
     bob.bank_account += bob.salary;
 
     if (month == 12){
-        bob.salary *= 1 + inflation / 100;
+        bob.salary *= 1 + INFLATION / 100;  // индексация
+    }
+}
+
+
+void alice_salary(int month)
+{
+    alice.bank_account += alice.salary;
+
+    if (month == 12){
+        alice.salary *= 1 + INFLATION / 100;  // индексация
     }
 }
 
@@ -47,17 +115,25 @@ void bob_expenses(int month)
 {
     bob.bank_account -= bob.monthly_expenses;
     bob.bank_account -= bob.rent;
+}
 
-    if (month == 12){
-        bob.monthly_expenses *= 1 + inflation / 100;
-        bob.rent *= 1 + inflation / 100;
-    }
+
+void alice_expenses(int month)
+{
+    alice.bank_account -= alice.monthly_expenses;
+    alice.bank_account -= mortgage.monthly_payment;
 }
 
 
 void bob_print()
 {
-    printf("Капитал Боба: %lld", bob.bank_account);
+    printf("Капитал Боба: %lld\n", bob.bank_account + bob_deposit.capital);
+}
+
+
+void alice_print()
+{
+    printf("Капитал Алисы: %lld", alice.bank_account + mortgage.apartment_cost);
 }
 
 
@@ -69,11 +145,16 @@ void simulation(int year, int month)
         {
             bob_salary(month);
             bob_expenses(month);
-            //printf("Прошел %d месяц\n", month);
+            deposit();
+
+            alice_salary(month);
+            alice_expenses(month);
+
+            inflation(month);
+
             month++;
         }
         month = 1;
-        //printf("Прошел %d год\n", year);
         year++;
     }
 }
@@ -82,8 +163,12 @@ void simulation(int year, int month)
 int main()
 {
     bob_init();
+    alice_init();
+
+    mortgage_payment();
 
     simulation(YEAR_START, MONTH_START);
 
     bob_print();
+    alice_print();
 }
