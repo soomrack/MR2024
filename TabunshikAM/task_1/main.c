f66851426b1d3ec380c9c0d0c238185fc8c0bca0
Табунщик Артём Михайлович <109157898+FOXFIREFOR@users.noreply.github.com>
2024-09-24 21:07:08 +0300

    Update main.c

--- TabunshikAM/task_1/main.c
+++ TabunshikAM/task_1/main.c
@@ -4,18 +4,17 @@
 
 typedef unsigned long long int money; // в копейках
 
-const double inflation_p = 9; // процент инфляции
-const double indexation_p = 7; // процент инфляции
-const double deposit_p = 20; // годовой процент депозита
+const double INFLATION_P = 9; // процент инфляции
+const double IDEXATION_P = 7; // процент инфляции
+const double DEPOSIT_P = 20; // годовой процент депозита
+const double MORTGAGE_P = 16; // ставка по ипотеке
+const int SIMULATION_TIME = 30; //длительность ипотеки
 
-const double mortgage_p = 16; // ставка по ипотеке
+const money FOOD_COST = 15 * 1000 * 100;
+const money TRANSPORT_COST = 3 * 1000 * 100;
+const money HOUSE_BILLS = 7 * 1000 * 100;
+const money PERSONAL = 10 * 1000 * 100;
 
-const int simulaton_time = 30; //длительность ипотеки
-
-const money food_cost = 15 * 1000 * 100;
-const money transport_cost = 3 * 1000 * 100;
-const money house_bills = 7 * 1000 * 100;
-const money personal = 10 * 1000 * 100;
 
 struct Mortgage
 {
@@ -24,9 +23,21 @@ money first_pay;
 money month_pay;
 };
 
+
 struct Rent
 {
 money month_pay;
+};
+
+struct Cat
+{
+    int year_of_purchase;
+    int month_of_purchase;
+    money cost;
+    money food_cost;
+    money medical_cost;
+    money funeral_cost;
+    int duration_of_life;
 };
 
 typedef struct Person
@@ -37,40 +48,55 @@ typedef struct Person
 
     struct Mortgage mortgage;
     struct Rent rent;
+    struct Cat cat;
 } Person;
 
-static Person alice;
-static Person bob;
+
+    Person alice;
+    Person bob;
+
 
 void alice_data()
 {
     alice.bank_account = (1000 * 1000) * 100;
     alice.earns = (200 * 1000) * 100;
-    alice.month_expences = food_cost + transport_cost + house_bills + personal;
+    alice.month_expences = FOOD_COST + TRANSPORT_COST + HOUSE_BILLS + PERSONAL + alice.cat.food_cost + alice.cat.medical_cost;
     alice.mortgage.sum = (13 * 1000 * 1000) * 100;
     alice.mortgage.first_pay = (1000 * 1000) * 100;
+
+    alice.cat.year_of_purchase = 2030;
+    alice.cat.month_of_purchase = 9;
+    alice.cat.cost = (50 * 1000) * 100;
+    alice.cat.food_cost = (3 * 1000) * 100;
+    alice.cat.medical_cost = (2 * 1000) * 100;
+    alice.cat.funeral_cost = (30 * 1000) * 100;
+    alice.cat.duration_of_life = 12;
+    
 }
+
 
 void bob_data()
 {
     bob.bank_account = (1000 * 1000) * 100;
     bob.earns = (200 * 1000) * 100;
     bob.rent.month_pay = (70 * 1000) * 100;
-    bob.month_expences = food_cost + transport_cost + house_bills + personal;
+    bob.month_expences = FOOD_COST + TRANSPORT_COST + HOUSE_BILLS + PERSONAL;
 }
+
 
 void mortgage_month_pay()
 {
-    double month_percentage = mortgage_p / (100 * 12);
+    double month_percentage = MORTGAGE_P / (100 * 12);
 
     alice.bank_account -= alice.mortgage.first_pay;
     
     alice.mortgage.month_pay = (alice.mortgage.sum - alice.mortgage.first_pay) \
-    * month_percentage / (1 - ( 1/ pow((1 + month_percentage), simulaton_time*12)));
+    * month_percentage / (1 - ( 1/ pow((1 + month_percentage), SIMULATION_TIME*12)));
 
     alice.mortgage.month_pay = round (alice.mortgage.month_pay);
     (money)alice.mortgage.month_pay;
 }
+
 
 void earns()
 {
@@ -78,12 +104,14 @@ void earns()
     bob.bank_account += bob.earns;
 }
 
+
 void deposit_percents()
 {
-    alice.bank_account *= (1 + deposit_p / (100*12));
+    alice.bank_account *= (1 + DEPOSIT_P / (100*12));
 
-    bob.bank_account *= (1 + deposit_p / (100*12));
+    bob.bank_account *= (1 + DEPOSIT_P / (100*12));
 }
+
 
 void month_expences()
 {
@@ -94,15 +122,24 @@ void month_expences()
     bob.bank_account -= bob.rent.month_pay;
 }
 
+void cat_keeping(int year, int month)
+{
+    if ((year == alice.cat.year_of_purchase) && (month == alice.cat.month_of_purchase));
+        alice.bank_account -= alice.cat.cost;
+    if ((year == alice.cat.year_of_purchase + alice.cat.duration_of_life) && (month == alice.cat.month_of_purchase));
+        alice.bank_account -= alice.cat.funeral_cost;
+}
+
 void inflation_and_indexation()
 {
-    alice.month_expences *= (1 + inflation_p / 100);
-    alice.earns *= (1 + indexation_p / 100);
+    alice.month_expences *= (1 + INFLATION_P / 100);
+    alice.earns *= (1 + IDEXATION_P / 100);
 
-    bob.month_expences *= (1 + inflation_p / 100);
-    bob.rent.month_pay *= (1 + inflation_p / 100);
-    bob.earns *= (1 + indexation_p / 100);
+    bob.month_expences *= (1 + INFLATION_P / 100);
+    bob.rent.month_pay *= (1 + INFLATION_P / 100);
+    bob.earns *= (1 + IDEXATION_P / 100);
 }
+
 
 void print()
 {
@@ -110,6 +147,7 @@ void print()
     printf("Alice mortgage = %lld\n", alice.mortgage.month_pay);
     printf("Alice capital = %lld\nBob capital = %lld\n", alice.bank_account, bob.bank_account);
 }
+
 
 void simulation()
 {
@@ -120,7 +158,7 @@ void simulation()
     int year = start_year;
     int month = start_month;
     
-    while( !((year == start_year + simulaton_time) && (month == start_month + 1)) ) {
+    while( !((year == start_year + SIMULATION_TIME) && (month == start_month + 1)) ) {
         //printf("Alice capital = %lld\nBob capital =   %lld\n\n", alice->bank_account/100, bob.bank_account/100);
         //получение зарплаты
         earns();
@@ -146,6 +184,7 @@ void simulation()
         }
     }
 }
+
 
 int main()
 {
