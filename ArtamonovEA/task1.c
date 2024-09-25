c920b24f25c22869aeb32d830e294014f53c24b0
Egor Artamonov <artegorr@gmail.com>
2024-09-25 15:39:11 +0300

    CatsDogs

--- ArtamonovEA/task1.c
+++ ArtamonovEA/task1.c
@@ -3,13 +3,20 @@
 #include <locale.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 
 float inflation_rate = 9.0;
+int duration = 30;
 
 // ------------------------------------------------------  Types  ----------------------------------------------------- //
 
 typedef long long int Money;
+
+typedef struct {
+    int year;
+    int month;
+} Date;
 
 typedef struct {
     Money cost;
@@ -24,39 +31,28 @@ typedef struct {
 } Deposit;
 
 typedef struct {
-    Money appartment_cost;
-    Money appartment_rent_cost;
+    Money rent_cost;
     Money food_cost;
     Money service_cost;
     Money personal_cost;
-} Goods;
+} Bills;
 
 typedef struct {
     char name[20];
     Money capital;
     Deposit deposit;
+    Mortage mortage;
+    Money appartment_cost;
     Money salary;
     Money annual_bonus;
-    int is_working;
-    Goods goods;
+    Bills bills;
 } Person;
 
 typedef struct {
-    const int start_year;
-    const int start_month;
-    const int end_year;
-    const int end_month;
-    int current_year;
-    int current_month;
-    int alice_fired_in_months;
-    int bob_fired_in_months;
-} Dates;
-
-typedef struct {
-    Person alice;
-    Person bob;
-    Mortage mortage;
-    Dates dates;
+    Person person;
+    Date start_date;
+    Date current_date;
+    Date end_date;
 } Simulation;
 
 // ----------------------------------------------------  Functions  ---------------------------------------------------- //
@@ -70,86 +66,150 @@ Mortage init_mortage(Money cost, int rate, int duration)
     return result;
 }
 
-Dates init_dates(int start_year, int start_month, int duration, int alice_fired_in_months, int bob_fired_in_months) 
+
+Simulation init_alice_simulation() 
 {
-    int end_year = start_year + duration;
-    int end_month = 12;
-
-    if (start_month - 1 == 0) {
-        end_year--;
-        end_month = 12;
-    } else {
-        end_month = start_month - 1;
-    }
-
-    Dates result = {
-        start_year, start_month, 
-        end_year, end_month, 
-        start_year, start_month, 
-        alice_fired_in_months, bob_fired_in_months
+    Bills bills = (Bills){
+        .rent_cost = 0, 
+        .food_cost = 20 * 1000, 
+        .service_cost = 30 * 1000, 
+        .personal_cost = 15 * 1000
     };
 
+    Mortage mortage = init_mortage(15 * 1000 * 1000, 20, 30);
+
+    Person person = (Person){
+        .name = "Alice", 
+        .capital = 1000 * 1000, 
+        .deposit = (Deposit){
+            .value = 0, 
+            .rate = 24
+        }, 
+        .mortage = mortage,
+        .bills = (Bills){
+            .rent_cost = 0,
+            .food_cost = 15 * 1000,
+            .personal_cost = 7 * 1000,
+            .service_cost = 10 * 1000
+        },
+        .salary = 200 * 1000,
+        .annual_bonus = 1000 * 1000,
+        .appartment_cost = 13 * 1000 * 1000
+    };
+
+    Date start = (Date) {
+        .year = 2024,
+        .month = 9
+    };
+
+    Date end = (Date) {
+        .year = start.year + duration,
+        .month = start.month - 1
+    };
+
+    if (end.month == 0) {
+        end.year--;
+        end.month = 12;
+    }
+
+    Simulation result = (Simulation){
+        .person = person,
+        .start_date = start,
+        .current_date = start,
+        .end_date = end
+    };
     return result;
 }
 
-Simulation init_simulation() 
+Simulation init_bob_simulation() 
 {
-    setlocale(LC_NUMERIC, "");
+    Person person = (Person){
+        .name = "Bob", 
+        .capital = 700 * 1000, 
+        .deposit = (Deposit){
+            .value = 0, 
+            .rate = 22
+        },
+        .mortage = (Mortage){
+            .cost = 0, 
+            .period = 0, 
+            .rate = 0, 
+            .payment = 0
+        },
+        .bills = (Bills){
+            .rent_cost = 30 * 1000,
+            .food_cost = 15 * 1000,
+            .personal_cost = 7 * 1000,
+            .service_cost = 10 * 1000
+        },
+        .salary = 500 * 1000,
+        .annual_bonus = 1000 * 1000,
+        .appartment_cost = 0
+    };
 
-    Deposit alice_deposit = {0, 24};
-    Goods alice_goods = {15 * 1000 * 1000, 0, 15 * 1000, 8 * 1000, 17 * 1000};
-    Person alice = {"Alice", 1000 * 1000, alice_deposit, 200 * 1000, 300 * 1000, 1};
+    Date start = (Date) {
+        .year = 2024,
+        .month = 9
+    };
 
-    Deposit bob_deposit = {0, 22};
-    Goods bob_goods = {0, 30 * 1000, 12 * 1000, 10 * 1000, 7 * 1000};
-    Person bob = {"Bob", 700 * 1000, bob_deposit, 300 * 1000, 500 * 1000, 1};
-    Dates dates = init_dates(2024, 1, 30, 57, 96);
+    Date end = (Date) {
+        .year = start.year + duration,
+        .month = start.month - 1
+    };
 
-    Mortage mortage = init_mortage(15 * 1000 * 1000, 30, 20);
+    if (end.month == 0) {
+        end.year--;
+        end.month = 12;
+    }
 
-    Simulation result = {alice, bob, mortage, dates};
+    Simulation result = (Simulation){
+        .person = person,
+        .start_date = start,
+        .current_date = start,
+        .end_date = end
+    };
     return result;
 }
 
-void add_inflation_to_goods(const int month, Goods* goods) 
+
+void add_inflation_to_bills(const int month, Bills* bills) 
 {
     if (month == 12) {
-        goods->appartment_cost *= 1 + inflation_rate / 100;
-        goods->appartment_rent_cost *= 1 + inflation_rate /100;
-        goods->food_cost *= 1 + inflation_rate / 100;
-        goods->service_cost *= 1 + inflation_rate / 100;
-        goods->personal_cost *= 1 + inflation_rate / 100;
+        bills->rent_cost *= 1 + inflation_rate /100;
+        bills->food_cost *= 1 + inflation_rate / 100;
+        bills->service_cost *= 1 + inflation_rate / 100;
+        bills->personal_cost *= 1 + inflation_rate / 100;
     };
 }
 
-void salary(Person* person, int month) 
-{   
-    if (person->is_working == 0) {
-        return;
-    }
 
+void salary(Person* person, int current_month) 
+{   
     person->capital += person->salary;
 
-    if (month == 12) {
+    if (current_month == 12) {
         person->capital += person->annual_bonus;
         person->salary *= 1 + inflation_rate / 100;
         person->annual_bonus *= 1 + inflation_rate / 100;
     };
 }
 
-void pay_mortgage(Person* person, Mortage* mortage) 
+
+void pay_mortgage(Person* person) 
 {
-    person->capital -= mortage->payment;
+    person->capital -= person->mortage.payment;
 }
+
 
 void pay_bills(Person* person) 
 {
 
-    person->capital -= person->goods.food_cost;
-    person->capital -= person->goods.service_cost;
-    person->capital -= person->goods.personal_cost;
-    person->capital -= person->goods.appartment_rent_cost;
+    person->capital -= person->bills.food_cost;
+    person->capital -= person->bills.service_cost;
+    person->capital -= person->bills.personal_cost;
+    person->capital -= person->bills.rent_cost;
 }
+
 
 void increase_deposit(Person* person) 
 {
@@ -158,11 +218,13 @@ void increase_deposit(Person* person)
     person->capital = 0;
 }
 
+
 void show_capital(Person* person) 
 {
     printf("%s\n", person->name);
     printf(" Capital is %.lld rub\n", person->capital);
 }
+
 
 void inflation_change() 
 {
@@ -171,65 +233,81 @@ void inflation_change()
     inflation_rate *= (100 + change_direction * change_value) * 1.0 / 100;
 }
 
+
+void have_cat(Simulation* simulation) {
+    if (simulation->current_date.year == 2028 && simulation->current_date.month == 9) {
+        simulation->person.capital -= 100 * 1000;
+        return;
+    }
+
+    if (simulation->current_date.year < 2030 
+                && simulation->current_date.month < 9) {
+        simulation->person.capital -= 10 * 1000;
+    }
+}
+
+
+void have_dog(Simulation* simulation) {
+    if (simulation->current_date.year == 2032 && simulation->current_date.month == 5) {
+        simulation->person.capital -= 30 * 1000;
+        return;
+    }
+
+    if (simulation->current_date.year < 2040 
+                && simulation->current_date.month < 12) {
+        simulation->person.capital += 15 * 1000;
+    }
+}
+
+
 void move_to_next_month(Simulation* simulation) 
 {
-    if (simulation->dates.current_month == 12) {
-        simulation->dates.current_year++;
-        simulation->dates.current_month = 1;
+    if (simulation->current_date.month == 12) {
+        simulation->current_date.year++;
+        simulation->current_date.month = 1;
         inflation_change();
 
     } else {
-        simulation->dates.current_month++;
-    }
-
-    simulation->dates.alice_fired_in_months--;
-    simulation->dates.bob_fired_in_months--;
-    
-    if (simulation->dates.alice_fired_in_months == 0) {
-        simulation->alice.is_working = 0;
-    }
-
-    if (simulation->dates.bob_fired_in_months == 0) {
-        simulation->bob.is_working = 0;
+        simulation->current_date.month++;
     }
 }
 
-void calculate_capital(Person* p, long components[], int components_count) 
+
+void calculate_capital(Person* person) 
 {
-    for (int i = 0; i < components_count; i++) {
-        p->capital += components[i];
-    }
+    person->capital += person->deposit.value;
+    person->capital += person->appartment_cost;
 }
+
 
 // ----------------------------------------------------  Simulation  --------------------------------------------------- //
 
+
 void run_simulation(Simulation simulation) 
 {
-    while( !(
-        simulation.dates.current_year == simulation.dates.end_year && 
-        simulation.dates.current_month == simulation.dates.end_month
-        ) ) {
-        salary(&simulation.alice, simulation.dates.current_month);
-        salary(&simulation.bob, simulation.dates.current_month);
+    while( !(simulation.current_date.year == simulation.end_date.year 
+            && simulation.current_date.month  == simulation.end_date.month) ) {
 
-        pay_mortgage(&simulation.alice, &simulation.mortage);
-        pay_bills(&simulation.alice);
-        pay_bills(&simulation.bob);
+        salary(&simulation.person, simulation.current_date.month);
 
-        increase_deposit(&simulation.alice);
-        increase_deposit(&simulation.bob);
+        pay_mortgage(&simulation.person);
+        pay_bills(&simulation.person);
 
-        add_inflation_to_goods(simulation.dates.current_month, &simulation.alice.goods);
-        add_inflation_to_goods(simulation.dates.current_month, &simulation.bob.goods);
+        if (strcmp(simulation.person.name, "Alice") != 0) {
+            have_cat(&simulation);
+            have_dog(&simulation);
+        }
+
+        increase_deposit(&simulation.person);
+
+        add_inflation_to_bills(simulation.current_date.month, &simulation.person.bills);
 
         move_to_next_month(&simulation);
     }
 
-    calculate_capital(&simulation.alice, (long[]) {simulation.alice.goods.appartment_cost, simulation.alice.deposit.value}, 2);
-    calculate_capital(&simulation.bob, (long[]) {simulation.bob.deposit.value}, 1);
+    calculate_capital(&simulation.person);
 
-    show_capital(&simulation.alice);
-    show_capital(&simulation.bob);
+    show_capital(&simulation.person);
 }
 
 
@@ -238,7 +316,15 @@ void run_simulation(Simulation simulation)
 int main() 
 {
     srand(time(NULL));
-    Simulation simulation = init_simulation();
-    run_simulation(simulation);
+    setlocale(LC_NUMERIC, "");
+
+    Simulation alice_simulation = init_alice_simulation();
+
+    run_simulation(alice_simulation);
+
+    Simulation bob_simulation = init_alice_simulation();
+
+    run_simulation(bob_simulation);
+
     return 0;
 }
