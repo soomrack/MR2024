02d2d05f20ff05061e707d3a08ed28511cf4cc13
Motyapls <matvleontiev@yandex.ru>
2024-09-30 13:37:57 +0300

    task_1.c

--- LeontievML/task_1.c
+++ LeontievML/task_1.c
@@ -4,34 +4,32 @@ const int ALICE_CAPITAL = 1000 * 1000 * 100;
 const int BOB_CAPITAL = 1000 * 1000 * 100;
 
 const int PRICE_HOME = 15 * 1000 * 1000 * 100;
-const int MORTGAGE_TIME = 30;  // годы
+const int MORTGAGE_TIME = 30; // годы
 const float MORTGAGE_RATE = 1.17;
-const int MONTH_PAY = 19959455;  // ежемесячный платеж по ипотеке
+const int MONTH_PAY = 19959455; // ежемесячный платеж по ипотеке
 
 const float INFLIATION = 1.08;
 const float INDEXATION = 1.04;
 const float DEPOSIT_RATE = 1.1;
 
-typedef long long int Money;  // копейки
+typedef long long int Money; // копейки
 
-
-struct Person {
+struct Person
+{
     unsigned long long int summ; // сумма процентов и стоимости квартиры по ипотеке
-    unsigned long int depozit; // деньги на вкладе
+    unsigned long int depozit;   // деньги на вкладе
     Money salary;
     Money cost_food;
     Money cost_entertainmants;
     Money cost_transport;
     Money cost_services;
     Money cost_home; // стоимость дома Алисы
-    Money account; // деньги на руках, не лежащие на вкладе
+    Money account;   // деньги на руках, не лежащие на вкладе
     Money rent;
 };
 
-
 struct Person alice;
 struct Person bob;
-
 
 void alice_init()
 {
@@ -46,7 +44,6 @@ void alice_init()
     alice.cost_home = PRICE_HOME;
 }
 
-
 void bob_init()
 {
     bob.depozit = BOB_CAPITAL;
@@ -59,51 +56,47 @@ void bob_init()
     bob.rent = 50 * 000;
 }
 
-
 void alice_salary(const int month)
 {
-    if (month == 1) alice.salary *= INDEXATION;
-    alice.account += alice.salary;
+    if (month == 1) {
+        alice.salary *= INDEXATION;
+        alice.account += alice.salary;
+    }
 }
-
 
 void bob_salary(const int month)
 {
-    if (month == 1) bob.salary *= INDEXATION;
-    bob.account += bob.salary;
+    if (month == 1) {
+        bob.salary *= INDEXATION;
+        bob.account += bob.salary;
+    }
 }
-
 
 void alice_expences(const int month)
 {
-    if (month == 1)
-    {
+    if (month == 1) {
         alice.cost_food *= INFLIATION;
         alice.cost_entertainmants *= INFLIATION;
         alice.cost_transport *= INFLIATION;
         alice.cost_services *= INFLIATION;
     }
-    
-    alice.account -= (alice.cost_food + alice.cost_entertainmants 
-    + alice.cost_transport + alice.cost_services);
-}
 
+    alice.account -= (alice.cost_food + alice.cost_entertainmants 
+        + alice.cost_transport + alice.cost_services);
+}
 
 void bob_expences(const int month)
 {
-    if (month == 1)
-    {
+    if (month == 1) {
         bob.cost_food *= INFLIATION;
         bob.cost_entertainmants *= INFLIATION;
         bob.cost_transport *= INFLIATION;
         bob.cost_services *= INFLIATION;
         bob.rent *= INFLIATION;
     }
-    
-    bob.account -= (bob.cost_food + bob.cost_entertainmants 
-    + bob.cost_transport + bob.cost_services + bob.rent);
-}
 
+    bob.account -= (bob.cost_food + bob.cost_entertainmants + bob.cost_transport + bob.cost_services + bob.rent);
+}
 
 void alice_depozit()
 {
@@ -112,7 +105,6 @@ void alice_depozit()
     alice.depozit += (alice.account * DEPOSIT_RATE / 12);
 }
 
-
 void bob_depozit()
 {
     bob.depozit += bob.account;
@@ -120,25 +112,21 @@ void bob_depozit()
     bob.depozit += (bob.account * DEPOSIT_RATE / 12);
 }
 
-
 void alice_mortgage(const int month)
 {
-    if (alice.summ > 0)
-    {
+    if (alice.summ > 0) {
         alice.summ -= MONTH_PAY;
         alice.account -= MONTH_PAY;
     }
     if (month == 1) alice.cost_home *= INFLIATION;
 }
 
-
 void bob_pets(const int year)
 {
-    if (year <= 2026) bob.account -= 2 * 1000;  // Боб завел хомяка
-    if (year <= 2030 && year > 2026) bob.account -= 5 * 1000;  // Боб завел кошку
-    if (year > 2030) bob.account -= 7 * 1000;  // Боб завел черепашку
+    if (year <= 2026) bob.account -= 2 * 1000; // Боб завел хомяка
+    if (year <= 2030 && year > 2026) bob.account -= 5 * 1000; // Боб завел кошку
+    if (year > 2030) bob.account -= 7 * 1000; // Боб завел черепашку
 }
-
 
 void print()
 {
@@ -151,20 +139,17 @@ void print()
         printf("A Bob's stratagy is more succesful\n");
 }
 
-
 void simulation()
 {
     // начало отсчета
     int year = 2024;
     int month = 9;
 
-    while (!(year == 2024 + MORTGAGE_TIME && month == 9))
-    {
+    while (!(year == 2024 + MORTGAGE_TIME && month == 9)) {
         alice_salary(month);
         alice_expences(month);
         alice_mortgage(month);
         alice_depozit();
-
 
         bob_salary(month);
         bob_expences(month);
@@ -172,16 +157,15 @@ void simulation()
         bob_depozit();
 
         month += 1;
-        if (month == 13)
-        {
+        if (month == 13) {
             month = 1;
             year += 1;
         }
     }
 }
 
-
-int main() {
+int main()  
+{
     alice_init();
     bob_init();
 
