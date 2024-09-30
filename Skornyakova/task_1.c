5561bd91bea45653147d9e6b2bccbed82f747bf9
Tiredelephant18 <147646766+Tiredelephant18@users.noreply.github.com>
2024-09-30 11:58:40 +0300

    Update task_1.c

--- Skornyakova/task_1.c
+++ Skornyakova/task_1.c
@@ -3,18 +3,15 @@
  
 typedef long long int  Money; 
 
-typedef struct 
-{
+typedef struct {
     Money sum;
     Money first_payment;
     Money monthly_payment;
     int rate;
-
-
 } Mortgage;
 
 
-typedef struct  { 
+typedef struct { 
     Money salary ; 
     Money capital;
     Money deposit;
@@ -22,15 +19,14 @@ typedef struct  {
     int deposit_rate; 
     Money apartment_payment;  // ежемесечная плата за проживание 
     Money others_expences;  // ежемесячные дополнительные  расходы 
-    Mortgage mortgage;
-     
+    Mortgage mortgage;   
 } Person;
 
 Person Alice;
 Person Bob; 
 Mortgage mortgage;
-void Alice_init () { 
-    
+void Alice_init () 
+{   
     Alice.wallet = 1000*1000; 
     Alice.salary = 260*1000; 
     Alice.deposit = 0;
@@ -40,75 +36,84 @@ void Alice_init () {
     Alice.wallet -= Alice.mortgage.first_payment;
     Alice.mortgage.sum = 15 * 1000 * 1000;
     Alice.mortgage.monthly_payment = 178209; // посчитан на сайте https://www.banki.ru/services/calculators/hypothec/
-
 } 
-void Bob_init(){
+void Bob_init()
+{
     Bob.wallet = 0; 
     Bob.salary = 260*1000; 
     Bob.others_expences = 40*1000; 
     Bob.deposit_rate = 0.2;
     Bob.deposit = 1000*1000;
     Bob.apartment_payment = 30*1000;  
-     }
+}
 
-void Alice_salary(int month){
+void Alice_salary(int month)
+{
   Alice.wallet += Alice.salary;
   if (month == 12)
     Alice.salary *= 1.08;
 }
 
-void Bob_salary(int month){
+void Bob_salary(int month)
+{
   Bob.wallet += Bob.salary;
    if (month == 12)
    Bob.salary *= 1.08;
-  
 }
 
-void Alice_others_expences(int month){
+void Alice_others_expences(int month)
+{
   Alice.wallet -= Alice.others_expences;
   if (month == 12)
    Alice.others_expences *=1.08;
  }
 
-void Bob_others_expences(int month){
+void Bob_others_expences(int month)
+{
   Bob.wallet -= Bob.others_expences;
   if (month == 12)
    Bob.others_expences *=1.08;
  }
 
-void Alice_accommodation_payment(){
+void Alice_accommodation_payment()
+{
  Alice.wallet -= Alice.mortgage.monthly_payment; 
 } 
 
-void Bob_accommodation_payment(int month){
+void Bob_accommodation_payment(int month)
+{
  Bob.wallet -= Bob.apartment_payment;
  if (month == 12)
    Bob.apartment_payment *= 1.08;
 } 
 
-void flat_price(int month){
+void flat_price(int month)
+{
   if(month == 12)
     Alice.mortgage.sum *= 1.08;  
 }
 
-void Alice_deposit(){
+void Alice_deposit()
+{
   Alice.deposit += Alice.wallet ;
   Alice.wallet = 0;
   if (Alice.deposit > 0)
   Alice.deposit *= 1.0167;
   }
 
-void Bob_deposit(){
+void Bob_deposit()
+{
   Bob.deposit += Bob.wallet ;
   Bob.wallet = 0;
   Bob.deposit *= 1.0167;
   }
     
-void Simulation(int start_year, int start_month){
+void Simulation(int start_year, int start_month)
+{
     int end_year = start_year + 25;
     int month = start_month;
     int year = start_year;
-    while(! ((year == end_year)&&(month == start_month )))    {
+    while( !((year == end_year) && (month == start_month ))) {
       Alice_salary(month);
       Alice_others_expences(month);
       Alice_accommodation_payment();
@@ -125,14 +130,12 @@ void Simulation(int start_year, int start_month){
         if (month == 13){ 
             year ++;
             month = 1;}
-      } 
-    
-    }
+      }
+ }
 void person_printf()
   {
     printf("Alice capital = %lld\n", Alice.deposit + Alice.mortgage.sum);
-    printf("Bob capital = %lld\n", Bob.deposit); 
-
+    printf("Bob capital = %lld\n", Bob.deposit);
   }
 
 int main(){
