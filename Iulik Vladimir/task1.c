4e09e27dc45b49645a56dd3b2bceff986e7d5f97
vna2re <67416519+vna2re@users.noreply.github.com>
2024-10-07 14:36:34 +0500

    Add files via upload

--- Iulik Vladimir/task1.c
+++ Iulik Vladimir/task1.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
 #include <math.h>
 
-typedef long long int Money; //  в копейках
+typedef long long int Money;  // в копейках
 
 int MONTH = 9;
 int YEAR = 2024;
-double INFLATION = 0.08;  //  процент годовой инфляции
-double DEP_PERCENT_BOB = 0.2; //  процент по вкладу Боба
-double DEP_PERCENT_ALICE = 0.2; //  процент по вкладу Алисы
-int FLAG = 1; //  Флаг для изменения процентной ставки
+double INFLATION = 0.08;  // процент годовой инфляции
+double DEP_PERCENT_BOB = 0.2;  // процент по вкладу Боба
+double DEP_PERCENT_ALICE = 0.2;  // процент по вкладу Алисы
+int FLAG = 1;  // Флаг для изменения процентной ставки
 
 Money IPOTEKA_AMOUNT;
 
 
-struct Person
-{
+struct Person {
     Money capital;
     Money salary;
-    Money home_cost;     //  стоимость квартиры
-    Money month_payment; //  ежемесячный платеж по ипотеке
+    Money home_cost;  // стоимость квартиры
+    Money month_payment;  // ежемесячный платеж по ипотеке
     Money food_expenses;
     Money utility_expenses;
     Money other_expenses;
-    Money rent;            //  арендная плата
-    Money initial_deposit; //  начальный депозит в банк, по которому начисляются проценты(меняется каждый год)
-    Money deposit;         //  депозит в банк, на который начисляются проценты
+    Money rent;  // арендная плата
+    Money initial_deposit;  // начальный депозит в банк, по которому начисляются проценты(меняется каждый год)
+    Money deposit;  // депозит в банк, на который начисляются проценты
 };
 
 
-struct Person alice; //  ипотека
-struct Person bob;   //  депозит
+struct Person alice;  // ипотека
+struct Person bob;  // депозит
 
 
 void alice_init()
@@ -61,8 +60,7 @@ void bob_init()
 void alice_salary(const int month)
 {
     alice.capital += alice.salary;
-    if (month == 12)
-    {
+    if (month == 12) {
         alice.salary += alice.salary * INFLATION;
     }
 }
@@ -71,8 +69,7 @@ void alice_salary(const int month)
 void alice_expenses(const int month)
 {
     alice.capital -= (alice.food_expenses + alice.utility_expenses + alice.other_expenses);
-    if (month == 12)
-    {
+    if (month == 12) {
         alice.food_expenses += alice.food_expenses * INFLATION;
         alice.utility_expenses += alice.utility_expenses * INFLATION;
         alice.other_expenses += alice.other_expenses * INFLATION;
@@ -84,8 +81,7 @@ void alice_ipoteka_pay(const int month)
 {
     alice.capital -= alice.month_payment;
     IPOTEKA_AMOUNT += alice.month_payment;
-    if (month == 12)
-    {
+    if (month == 12) {
         alice.home_cost += alice.home_cost * INFLATION;
     }
 }
@@ -93,8 +89,7 @@ void alice_ipoteka_pay(const int month)
 
 void alice_deposit_accrual(const int month, Money capital)
 {
-    if (month == 9)
-    {
+    if (month == 9) {
         alice.initial_deposit = alice.deposit;
     }
     alice.deposit += alice.initial_deposit * (DEP_PERCENT_ALICE / 12);
@@ -105,8 +100,7 @@ void alice_deposit_accrual(const int month, Money capital)
 void bob_salary(const int month)
 {
     bob.capital += bob.salary;
-    if (month == 12)
-    {
+    if (month == 12) {
         bob.salary += bob.salary * INFLATION;
     }
 }
@@ -115,8 +109,7 @@ void bob_salary(const int month)
 void bob_expenses(const int month)
 {
     bob.capital -= (bob.food_expenses + bob.utility_expenses + bob.other_expenses);
-    if (month == 12)
-    {
+    if (month == 12) {
         bob.food_expenses += bob.food_expenses * INFLATION;
         bob.utility_expenses += bob.utility_expenses * INFLATION;
         bob.other_expenses += bob.other_expenses * INFLATION;
@@ -127,8 +120,7 @@ void bob_expenses(const int month)
 void bob_rent_pay(const int month)
 {
     bob.capital -= bob.rent;
-    if (month == 12)
-    {
+    if (month == 12) {
         bob.rent += bob.rent * INFLATION;
     }
 }
@@ -136,12 +128,10 @@ void bob_rent_pay(const int month)
 
 void bob_deposit_accrual(const int month, Money capital)
 {
-    if (month == 9)
-    {
+    if (month == 9) {
         bob.initial_deposit = bob.deposit;
     }
-    if (bob.deposit >= 6*1000*1000*100 && FLAG == 1)
-    {
+    if (bob.deposit >= 6*1000*1000*100 && FLAG == 1) {
         DEP_PERCENT_BOB += 0.01;
         FLAG = 0;
     }
@@ -152,16 +142,13 @@ void bob_deposit_accrual(const int month, Money capital)
 
 void print_coclusion(const Money bob_loot, const Money alice_loot)
 {
-    if (alice_loot > bob_loot)
-    {
+    if (alice_loot > bob_loot) {
         printf("Выгоднее стратегия Алисы\n");
     }
-    else if (alice_loot < bob_loot)
-    {
+    else if (alice_loot < bob_loot) {
         printf("Выгоднее стратегия Боба\n");
     }
-    else
-    {
+    else {
         printf("Обе стратегии выгодные\n");
     }
     printf("Боб сэкономил и заработал за 30 лет %lld рублей \n", (bob_loot) / 100);
@@ -181,8 +168,7 @@ void simulation(int month, int year)
     bob.deposit = bob.capital;
     bob.capital -= bob.deposit;
 
-    while (!((year == end_year) && (month == MONTH)))
-    {
+    while (!((year == end_year) && (month == MONTH))) {
         alice_salary(month);
         alice_expenses(month);
         alice_ipoteka_pay(month);
@@ -194,14 +180,11 @@ void simulation(int month, int year)
         bob_deposit_accrual(month, bob.capital);
 
         month += 1;
-        if (month == 13)
-        {
+        if (month == 13) {
             year += 1;
             month = 1;
         }
     }
-
-    print_coclusion(bob.capital + bob.deposit, alice.capital + alice.home_cost + alice.deposit);
 }
 
 
@@ -211,5 +194,8 @@ int main()
     bob_init();
 
     simulation(MONTH, YEAR);
+
+    print_coclusion(bob.capital + bob.deposit, alice.capital + alice.home_cost + alice.deposit);
+
     return 0;
 }
