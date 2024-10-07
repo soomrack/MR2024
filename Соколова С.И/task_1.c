a8aab0b5cf3777e22dc64c20beb4f5b89e60ca13
Sonya-bot222 <sonyasokolova04@gmail.com>
2024-10-07 12:53:57 +0300

    Add files via upload

--- Соколова С.И/task_1.c
+++ Соколова С.И/task_1.c
@@ -3,48 +3,65 @@
 typedef long long int Money;
 
 struct Mortgage{
-    Money first_pay; // первый взнос
-    Money pay_a_mmonth; // ежемесячный платеж
-    Money sum; // сумма ипотеки
+    Money first_pay; 
+    Money pay_a_mmonth; 
+    Money sum; 
     double rate;
  };
  struct Person {
-    Money account; //вклад
-    Money salary; // зп 
-    Money expenses; // расходы
-    Money house; // КУ
-    Money rent; // аренда дома
-    Money food; // расходы на еду
+    Money account; 
+    Money salary;
+    Money expenses; 
+    Money house; 
+    Money rent;
+    Money food; 
+    Money cat;
     struct Mortgage mortgage; 
-} alice, bob;
+};
 
- void alice_init() {
-    alice.account = 1000*1000;
-    alice.salary = 250*1000;
-    alice.food = 15*1000;
-    alice.expenses = 10*1000;
-    alice.house = 5*1000;
 
-    alice.mortgage.sum = 15*1000*1000;
+struct Person alice;
+struct Person bob;
+
+
+void alice_init() {
+    alice.account = 1000 * 1000;
+    alice.salary = 250 * 1000;
+    alice.food = 15 * 1000;
+    alice.expenses = 10 * 1000;
+    alice.house = 5 * 1000;
+
+    alice.mortgage.sum = 15 * 1000 * 1000;
     alice.mortgage.rate = 0.17;
     alice.mortgage.pay_a_mmonth = 199595; // посчитано в калькуляторе ипотеки
-    alice.mortgage.first_pay = 1000*1000; 
+    alice.mortgage.first_pay = 1000 * 1000; 
     alice.account -= alice.mortgage.first_pay;  
  }
 
- void alice_salary(const int month, const int year) {
+
+void alice_account(const int month, const int year)
+{
+    alice.account *= 1 + 0.20 / 12;
+}
+
+
+void alice_salary(const int month, const int year) 
+{
     alice.account += alice.salary;
 
     if (month == 12) {
-        alice.salary *= 1.08; // индексация зарплаты
+        alice.salary *= 1.08;
     }
- }
+}
 
-void alice_mortgage_pay() {
+void alice_mortgage_pay() 
+{
     alice.account -= alice.mortgage.pay_a_mmonth;
 }
 
-void alice_food_pay(const int month, const int year) {
+
+void alice_food_pay(const int month, const int year)
+{
    alice.account -= alice.food;
 
    if (month == 12) {
@@ -52,7 +69,9 @@ void alice_food_pay(const int month, const int year) {
    }
 }
 
-void alice_house_pay(const int month, const int year) {
+
+void alice_house_pay(const int month, const int year) 
+{
    alice.account -= alice.house;
 
    if (month == 12) {
@@ -60,43 +79,55 @@ void alice_house_pay(const int month, const int year) {
    }
 }
 
-void alice_expenses_pay(const int month, const int year) {
+
+void alice_expenses_pay(const int month, const int year) 
+{
    alice.account -= alice.expenses;
 
    if (month == 12) {
        alice.expenses *= 1.08;
    }
 }
-void alice_account(const int month, const int year) {;
 
-   if (month == 12) {
-      alice.account *= 1.2; // + 20% годовых на остаток на вкладе
-   }
-}
 
 void alice_print()
 {
     printf("Alice account = %lld RUB\n", alice.account);
 }
 
-void bob_init() {
-   bob.account = 1000*1000;
-   bob.salary = 250*1000;
-   bob.rent = 70*1000;
-   bob.food = 15*1000;
-   bob.expenses = 10*1000;
-   bob.house = 5*1000;
+void bob_init() 
+{
+   bob.account = 1000 * 1000;
+   bob.salary = 250 * 1000;
+   bob.rent = 70 * 1000;
+   bob.food = 15 * 1000;
+   bob.expenses = 10 * 1000;
+   bob.house = 5 * 1000;
+   bob.cat = 20 * 1000; 
 }
 
- void bob_salary(const int month, const int year) {
+
+void bob_account(const int month, const int year) 
+{
+   bob.account *= 1+0.2/12;
+}
+
+
+void bob_salary(const int month, const int year) 
+{
     bob.account += bob.salary;
 
     if (month == 12) {
-        bob.salary *= 1.08; // индексация зарплаты
+        bob.salary *= 1.08;
     }
- }
+    else if (year == 2030) {
+      bob.salary = 300 * 1000;
+    }
+}
 
-void bob_food_pay(const int month, const int year) {
+
+void bob_food_pay(const int month, const int year) 
+{
    bob.account -= bob.food;
 
    if (month == 12) {
@@ -104,7 +135,8 @@ void bob_food_pay(const int month, const int year) {
    }
 }
 
-void bob_house_pay(const int month, const int year) {
+void bob_house_pay(const int month, const int year) 
+{
    bob.account -= bob.house;
 
    if (month == 12) {
@@ -112,27 +144,33 @@ void bob_house_pay(const int month, const int year) {
    }
 }
 
-void bob_expenses_pay(const int month, const int year) {
+void bob_expenses_pay(const int month, const int year) 
+{
    bob.account -= bob.expenses;
 
    if (month == 12) {
       bob.expenses *= 1.08;
    }
 }
-void bob_account(const int month, const int year) {
 
-      bob.account *= 1+0.2/12; // + 20% годовых на остаток на вкладе
-   
-}
 
-void bob_rent(const int month, const int year) {
+void bob_rent(const int month, const int year) 
+{
    bob.account -= bob.rent;
 
    if (month == 12) {
       bob.rent *= 1.08;
    }
-
 }
+
+
+void bob_buy_a_cat(const int month, const int year) 
+{
+    if ((year == 2030) && (month == 3)) {
+       bob.account -= bob.cat;
+    }
+}
+
 
 void bob_print()
 {
@@ -140,7 +178,8 @@ void bob_print()
 }
 
 
-void simulation() {
+void simulation() 
+{
    int year = 2024;
    int month = 9;
 
@@ -158,6 +197,7 @@ void simulation() {
       bob_house_pay(month,year);
       bob_expenses_pay(month,year);
       bob_rent(month,year);
+      bob_buy_a_cat(month,year);
 
       month += 1;
       if (month == 13) {
@@ -171,12 +211,11 @@ int main()
 {
     alice_init();
     
-     bob_init();
+    bob_init();
 
     simulation();
 
     alice_print();
 
     bob_print();
-
 }
