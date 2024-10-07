0ad063ab01dfcd03c291d447fb07ba2349fb6b75
creper89 <creper_guy89@mail.ru>
2024-10-07 19:03:40 +0900

    Added printf

--- Bryuhanov Denis/exercise_2.c
+++ Bryuhanov Denis/exercise_2.c
@@ -2,7 +2,7 @@
 
     const int height = 5; // количество строк
     const int lenght = 4; // количество столбцов
-    double array[5 * 4]; // Наша матрица
+    double array1[5 * 4], array2[5 * 4]; // Наша матрица
 
 
 void array_init(double *arr, int i, int j)
@@ -16,25 +16,23 @@ void array_init(double *arr, int i, int j)
 }
 
 
-
-
 void array_output(double *arr, int i, int j)
 {
     for (int k = 0; k < i; k++){
         for (int l = 0; l < j; l++){
             printf("%.2f\t", arr[k * j + l]); 
         }
-        printf("\n"); 
+        printf("\n\n"); 
     }
 }
 
 
 int main()
 {
-
-    array_init(array, lenght, height);
-    array_output(array, lenght, height);
-  
+    array_init(array1, lenght, height);
+    array_init(array1, lenght, height);
+    array_output(array1, lenght, height);
     
+    printf
     return 0;
 }
