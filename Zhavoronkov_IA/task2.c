a81546803107739c0d941091e37d889084ee4887
il-zhav <ilya.zhavoronkov.2004@mail.ru>
2024-10-13 17:40:57 +0300

    Update task2.c
    Some changes in the code. The following functions have been added: C = A + B; C = A - B; A = A * k.

--- Zhavoronkov_IA/task2.c
+++ Zhavoronkov_IA/task2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <string.h>
 
 
 struct Matrix
@@ -11,6 +12,8 @@ struct Matrix
 };
 struct Matrix A;
 struct Matrix B;
+struct Matrix C;
+struct Matrix C2;
 
 
 void error()
@@ -33,10 +36,29 @@ int matrix_print(const struct Matrix A)
 }
 
 
+// Выделение памяти
+void malloc_give (struct Matrix* A)
+{
+    A->data = malloc(A->cols * A->rows * sizeof(double));
+}
+
+
+// Проверка на успешное выделение памяти
+int malloc_check (const struct Matrix* A)
+{
+    if (A->data == NULL) {
+        error();
+        printf("haha");
+        return 1; 
+    }
+    return 0;
+}
+
+
 // A = A + B
 int matrix_add (struct Matrix A, struct Matrix B)
 {
-    if (!((A.cols == B.cols) && (A.rows == B.rows ))) {
+    if ((A.cols != B.cols) || (A.rows != B.rows )) {
         error();
         return 1;
     }
@@ -48,22 +70,60 @@ int matrix_add (struct Matrix A, struct Matrix B)
 }
 
 
-// Выделение памяти
-void malloc_give (struct Matrix* A)
+// C = A + B
+struct Matrix matrix_sum(const struct Matrix A, const struct Matrix B)
 {
-    A->data = malloc(A->cols*A->rows*sizeof(double));
-    //return A.data;
+    if ((A.cols != B.cols) || (A.rows != B.rows )) {
+        error();
+        exit;
+    }
+
+    struct Matrix C;
+    C.cols = A.cols;
+    C.rows = A.rows;
+    malloc_give(&C);
+
+    if (malloc_check(&C)) {
+        exit;
+    } 
+
+    memcpy(C.data, A.data, C.cols * C.rows * sizeof(double));
+    matrix_add(C, B); 
+    return C;
 }
 
 
-// Проверка на успешное выделение памяти
-int malloc_check (const struct Matrix A)
+// C = A - B 
+struct Matrix matrix_min(const struct Matrix A, const struct Matrix B)
 {
-    if (A.data == NULL) {
+    if ((A.cols != B.cols) || (A.rows != B.rows )) {
         error();
-        return 1; 
+        exit;
     }
-    return 0;
+
+    struct Matrix C;
+    C.cols = A.cols;
+    C.rows = A.rows;
+    malloc_give(&C);
+
+    if (malloc_check(&C)) {
+        exit;
+    }
+    
+    for (size_t idx = 0; idx < A.cols * A.rows; ++idx) {    
+        C.data[idx] = A.data[idx] - B.data[idx];
+    }
+    return C;
+}
+
+
+// A = A * k
+void matrix_k (struct Matrix A, double k)
+{   
+    malloc_check(&A);
+    for (size_t idx = 0; idx < A.cols * A.rows; ++idx) {
+		A.data[idx] *= k;
+	}
 }
 
 
@@ -73,16 +133,16 @@ int main()
     A.rows = 3;
     B.cols = 3;
     B.rows = 3;
-
+    
     malloc_give(&A);
     malloc_give(&B);
-
-    malloc_check(A);
-    malloc_check(B);
     
+    if (malloc_check(&A) || malloc_check(&B)) {
+        return 1; // Завершение программы в случае ошибки
+    } 
 
     *(A.data) = 1;
-    A.data [1] = 2;
+    A.data [1] = -2;
     *(A.data + 2) = 6.0;
     A.data[5] = 4.0;
 
@@ -94,8 +154,21 @@ int main()
     matrix_add(A,B);
     matrix_print(A);
 
+    printf("______________\n");
+    printf("______________\n");
+    C = matrix_sum(A,B);
+    matrix_print(C);
+
+    C2 = matrix_min(A,B);
+    matrix_print(C2);
+
+    matrix_k(A,10);
+    matrix_print(A);
+
     free(A.data);
     free(B.data);
+    free(C.data);
+    free(C2.data);
 }
 
 
