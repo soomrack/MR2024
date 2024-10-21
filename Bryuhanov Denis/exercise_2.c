403ca70f3df7690d9536579f9fb4985b799024b4
creper89 <creper_guy89@mail.ru>
2024-10-22 07:49:09 +0900

    Added new functions
    Добавлено возвдение матриц в степень, копирование матриц и вывод матриц

--- Bryuhanov Denis/exercise_2.c
+++ Bryuhanov Denis/exercise_2.c
@@ -12,18 +12,20 @@ typedef struct
 //Необходимые функции матриц
 //Инициализация матрицы +
 //Заполнение матрицы +
+//Копирование матрицы + 
+//Вывод матрицы +
 //Сложение матриц +
 //Вычитание матриц + 
-//Умножение матриц +
 //Умножение матрицы на число +
-//Вывод матрицы
+//Деление матрицы на число + 
+//Умножение матриц +
+//Степень матрицы (натуральное число) +
+//Нахождение определителя матрицы
 //Транспонирование матрицы 
 //Деление матриц
-//Деление матрицы на число + 
 //Экспонента матрицы
-//Степень матрицы
-//Нахождение определителя матрицы
-//Копирование матрицы
+
+
 
 
 void matrix_init(Matrix *A, const int rows, const int cols)
@@ -42,11 +44,31 @@ void matrix_fill(Matrix *A)
 }
 
 
+void matrix_clone(Matrix *result, Matrix A)
+{
+    matrix_init(&*result, A.rows, A.cols);
+    for (size_t i = 0; i < A.rows * A.cols; i++) {
+        result->data[i] = A.data[i];
+    }
+    
+}
+
+
 void matrix_sum(Matrix *result, Matrix A, Matrix B)
 {
     if (A.rows == B.rows & A.cols == B.cols) {
         for (size_t i = 0; i < A.rows * A.cols; i++) {
             result->data[i] = A.data[i] + B.data[i];   
+        }
+    }
+}
+
+
+void matrix_sub(Matrix *result, Matrix A, Matrix B)
+{
+    if (A.rows == B.rows & A.cols == B.cols) {
+        for (size_t i = 0; i < A.rows * A.cols; i++) {
+            result->data[i] = A.data[i] - B.data[i];   
         }
     }
 }
@@ -71,7 +93,7 @@ void matrix_div_num(Matrix *result, Matrix A, double num)
 void matrix_mul(Matrix *result, Matrix A, Matrix B)
 {
     if (A.cols == B.rows) {
-        matrix_int(&result, A.rows, B.cols);
+        matrix_init(&*result, A.rows, B.cols);
         for (size_t row = 0; row < A.rows; row++) {
             for (size_t col = 0; col < B.cols; col++) {
                 result->data[row * result->cols + col] = 0;
@@ -84,17 +106,36 @@ void matrix_mul(Matrix *result, Matrix A, Matrix B)
 }
 
 
-void matrix_sub(Matrix *result, Matrix A, Matrix B)
-{
-    if (A.rows == B.rows & A.cols == B.cols) {
-        for (size_t i = 0; i < A.rows * A.cols; i++) {
-            result->data[i] = A.data[i] - B.data[i];   
+void matrix_pow(Matrix *result, Matrix A, int power){
+    if (A.cols == A.rows && power > 0) {
+        for (size_t i = 1; i < power; i++) {
+            matrix_mul(&*result, *result, A);
         }
     }
 }
 
 
+void matrix_print(Matrix A)
+{   
+    for (size_t i = 0; i < A.cols * A.rows; i++) {
+        if ((i) % A.cols == 0) {
+            printf("|");
+        }
+        printf("%.2f|", A.data[i]);
+        if ((i) % A.cols == A.cols - 1) {
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+
 int main()
 {
-
+    Matrix first_matrix;
+    matrix_init(&first_matrix, 3, 3);
+    matrix_fill(&first_matrix);
+    matrix_print(first_matrix);
+    matrix_pow(&first_matrix, first_matrix, 3);
+    matrix_print(first_matrix);
 }
