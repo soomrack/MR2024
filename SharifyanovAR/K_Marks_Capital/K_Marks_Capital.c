ed75bde5a33d867211caea9120e93c4e4b8cf9fe
Amir <93887854+any-questions@users.noreply.github.com>
2024-10-23 15:43:34 +0300

    Add files via upload

--- SharifyanovAR/K_Marks_Capital/K_Marks_Capital.c
+++ SharifyanovAR/K_Marks_Capital/K_Marks_Capital.c
@@ -2,39 +2,39 @@
 #include<math.h>
 
 
-typedef long long int money;
+typedef long long int Money;
 
 
 struct animal {
-    money buy;
-    money cost;
+    Money buy;
+    Money cost;
     int lifetime; // months
 }cat, parrot;
 
 struct dude {
-    money deposit;
-    money capital;
-    money salary;
-    money expenses;
-    money unx_expeses; 
-    money flat_payment;
-    money overpayment;
+    Money deposit;
+    Money capital;
+	Money salary;
+	Money expenses;
+	Money unx_expeses; 
+	Money flat_payment;
+	Money overpayment;
     struct animal cat;
     struct animal parrot;
 }Alice, Bob;
 
-const money START_DEPOSIT = 0;
-const money START_CAPITAL = 1100000 *100;
-const money INIT_SALARY = 270000 * 100;
-const money INIT_EXPENSES = 50000 * 100;
-const money INIT_UNX_EXPENSES = 10000 * 100;
-const money FLAT_PRICE = 10650000 * 100;
-const money RENT_PRICE = 45000 * 100;
-const money MORTRAGE_MOUNTH_PRICE = 170000 * 100;
-const money MORTRAGE_FIST_PAYMENT = 106500 * 100;
+const Money START_DEPOSIT = 0;
+const Money START_CAPITAL = 1100000 *100;
+const Money INIT_SALARY = 270000 * 100;
+const Money INIT_EXPENSES = 50000 * 100;
+const Money INIT_UNX_EXPENSES = 10000 * 100;
+const Money FLAT_PRICE = 10650000 * 100;
+const Money RENT_PRICE = 45000 * 100;
+const Money MORTRAGE_MOUNTH_PRICE = 170000 * 100;
+const Money MORTRAGE_FIST_PAYMENT = 106500 * 100;
 
-const money ANIMAL_BUY_COST = 10000 * 100;
-const money ANIMAL_KEEPING_COST = 2000 * 100; // per month
+const Money ANIMAL_BUY_COST = 10000 * 100;
+const Money ANIMAL_KEEPING_COST = 2000 * 100; // per month
 
 const int SIMULATION_DURATION = 30;
 const int START_YEAR = 2024;
@@ -45,24 +45,24 @@ double invest_rate = 0.18;
 
 void Alice_init() {
     Alice.deposit = START_DEPOSIT;
-    Alice.capital = START_CAPITAL- MORTRAGE_FIST_PAYMENT;
-    Alice.salary = INIT_SALARY;
-    Alice.expenses = INIT_EXPENSES;
-    Alice.unx_expeses = INIT_UNX_EXPENSES;
-    Alice.flat_payment = MORTRAGE_MOUNTH_PRICE;
+	Alice.capital = START_CAPITAL- MORTRAGE_FIST_PAYMENT;
+	Alice.salary = INIT_SALARY;
+	Alice.expenses = INIT_EXPENSES;
+	Alice.unx_expeses = INIT_UNX_EXPENSES;
+	Alice.flat_payment = MORTRAGE_MOUNTH_PRICE;
 	//Alice.overpayment = MORTRAGE_MOUNTH_PRICE * SIMULATION_DURATION - FLAT_PRICE;
 }
 
 void Bob_init() {
     Bob.deposit = START_DEPOSIT;
-    Bob.capital = START_CAPITAL;
-    Bob.salary = INIT_SALARY;
-    Bob.expenses = INIT_EXPENSES;
-    Bob.unx_expeses = INIT_UNX_EXPENSES;
-    Bob.flat_payment = RENT_PRICE;
+	Bob.capital = START_CAPITAL;
+	Bob.salary = INIT_SALARY;
+	Bob.expenses = INIT_EXPENSES;
+	Bob.unx_expeses = INIT_UNX_EXPENSES;
+	Bob.flat_payment = RENT_PRICE;
     Bob.cat.buy = ANIMAL_BUY_COST;
     Bob.cat.cost = ANIMAL_KEEPING_COST;
-    Bob.cat.lifetime = 180;
+    Bob.cat.lifetime = 96;
     Bob.parrot.buy = ANIMAL_BUY_COST;
     Bob.parrot.cost = ANIMAL_KEEPING_COST;
     Bob.parrot.lifetime = 600;
@@ -158,25 +158,37 @@ void bob_expenses(const int month, const int year)
     if (month == 1) {
         Bob.expenses *= (1. + inflation);
     }
-    if ((month >= 3 && year >= 2027) && !(year == (2027 + cat.lifetime / 12) && month == (cat.lifetime % 12 + 3))) {
+    Bob.capital -= Bob.expenses;
+}
+
+void bob_animal_cost(const int month, const int year)
+{
+    if ((month >= 3 && year >= 2027) && !(year == cat.lifetime / 12 && month == cat.lifetime % 12 + 3)) {
         Bob.capital -= Bob.cat.cost;
     }
-    if ((month >= 6 && year >= 2034) && !(year == (2034 + parrot.lifetime / 12) && month == (parrot.lifetime % 12 + 6))) {
+    if ((month >= 6 && year >= 2034) && !(year == parrot.lifetime / 12 && month == parrot.lifetime % 12 + 6)) {
         Bob.capital -= Bob.parrot.cost;
     }
-    Bob.capital -= Bob.expenses;
+    if (month == 1) {
+        Bob.parrot.cost *= (1. + inflation);
+        Bob.cat.cost *= (1. + inflation);
+    }
+}
+
+void bob_animal_buy(const int month, const int year)
+{
+    if (month == 3 && year == 2027) {
+        Bob.capital -= Bob.cat.buy;
+    }
+    if (month == 6 && year == 2034) {
+        Bob.capital -= Bob.parrot.buy;
+    }
 }
 
 void bob_unx_expenses(const int month, const int year)
 {
     if (month == 1) {
         Bob.unx_expeses *= (1. + inflation);
-    }
-    if (month == 3 && year == 2027) {
-        Bob.capital -= Bob.cat.buy;
-    }
-    if (month == 6 && year == 2034) {
-        Bob.capital -= Bob.parrot.buy;
     }
     Bob.capital -= Bob.unx_expeses;
 }
@@ -246,10 +258,9 @@ void print_output()
 {
     printf("Results for %d.%d are:\n", START_MONTH, START_YEAR + SIMULATION_DURATION);
     printf(".......................\n");
-	
     alice_print();
     bob_print();
-	
+
     conclusion();
 }
 
@@ -257,10 +268,9 @@ int main()
 {
     Alice_init();
     Bob_init();
-	
+
     simulation();
-	
+
     print_output();
-	
     return 0;
 }
