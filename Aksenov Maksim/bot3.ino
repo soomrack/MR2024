49eb9e53255b156d2b23dcdef11a66874f62b9bf
Maksikanchik <147877250+Maksikanchik@users.noreply.github.com>
2024-11-25 15:59:10 +0300

    Update bot3.ino

--- Aksenov Maksim/bot3.ino
+++ Aksenov Maksim/bot3.ino
@@ -8,15 +8,15 @@
 
 #define PIN_PIEZO 9
 
-typedef unsigned int Colour;
+typedef unsigned int Colour; 
 
-Colour black, white;
+Colour black, white; // значения сигналов полученных при каллибровке для белого и черного цвета
 
-Colour right_sensor, left_sensor;
+Colour right_sensor, left_sensor; //текущие значения правого и оевого датчика
 
-Colour lastseen;
+Colour lastseen; // крайнее значение сигнала перед съездом с линии
 
-Colour border;
+Colour border; //Среднее арефметическое значение между белым и черным цветом
 
 unsigned int search_time = 0;
 
@@ -77,12 +77,12 @@ void leftward()  //поворот налево
     digitalWrite(PIN_DIR_R, 1);
     digitalWrite(PIN_DIR_L, 0);
 
-    for (int i = 0; i <= 150; i += 6) {
-        analogWrite(PIN_PWR_R, i);
+    for (int idx = 0; idx <= 150; idx += 6) {
+        analogWrite(PIN_PWR_R, idx);
     }
 
-    for (int i = 0; i <= 150; i += 6) {
-        analogWrite(PIN_PWR_L, i);
+    for (int idx = 0; idx <= 150; idx += 6) {
+        analogWrite(PIN_PWR_L, idx);
     }
 
     search_time = 0;
@@ -94,53 +94,32 @@ void rightward()  //поворот направо
     digitalWrite(PIN_DIR_R, 0);
     digitalWrite(PIN_DIR_L, 1);
 
-    for (int i = 0; i <= 150; i += 6) {
-        analogWrite(PIN_PWR_R, i);
+    for (int idx = 0; idx <= 150; idx += 6) {
+        analogWrite(PIN_PWR_R, idx);
     }
 
-    for (int i = 0; i <= 150; i += 6) {
-        analogWrite(PIN_PWR_L, i);
+    for (int idx = 0; idx <= 150; idx += 6) {
+        analogWrite(PIN_PWR_L, idx);
     }
 
     search_time = 0;
 }
 
-
-void out_of_line()   //сход с линии     
+void out_of_line_left()   //сход с линии влево     
 {
-    search_time++;
-    delay(1);
-
-    if (lastseen > border) {
-        //Велчины 50 и 250 подобраны эмпирически
-        digitalWrite(PIN_DIR_R, 0);
-        analogWrite(PIN_PWR_R, 250);
-        digitalWrite(PIN_DIR_L, 0);
-        analogWrite(PIN_PWR_L, 50);
-
-        if (search_time > 5000) {
-            stop();
-        }
-    }
-
-    else {
-        //Велчины 50 и 250 подобраны эмпирически
-        digitalWrite(PIN_DIR_R,0);
-        analogWrite(PIN_PWR_R,50);
-        digitalWrite(PIN_DIR_L,0);
-        analogWrite(PIN_PWR_L,250);
-
-        if (search_time > 5000) {
-            stop();
-        }
-    }
+    //Данные скорости двигателей 50 для правого и 250 для левого подобраны на основе опытных данных, чтобы выполнялся задний поворот вправо
+    digitalWrite(PIN_DIR_R, 0);
+    analogWrite(PIN_PWR_R, 250);
+    digitalWrite(PIN_DIR_L, 0);
+    analogWrite(PIN_PWR_L, 50);
 }
-
-
-void setup() 
+void out_of_line_right()   //сход с линии вправо   
 {
-    pin_initialization();
-    calibration();   
+    //Данные скорости двигателей 50 для правого и 250 для левого подобраны на основе опытных данных, чтобы выполнялся задний поворот влево
+    digitalWrite(PIN_DIR_R,0);
+    analogWrite(PIN_PWR_R,50);
+    digitalWrite(PIN_DIR_L,0);
+    analogWrite(PIN_PWR_L,250);
 }
 
 
@@ -155,11 +134,17 @@ void stop()
 
     search_time = 0;
 
-    while (!digitalRead(PIN_BTN)) {}
+    if (digitalRead(PIN_BTN)) {
+        exit(1); 
+    }  
 
-    delay (500);
+}
 
-    exit(1);   
+
+void setup() 
+{
+    pin_initialization();
+    calibration();   
 }
 
 
@@ -181,8 +166,25 @@ void loop()
     }
   
     else if (left_sensor < border && right_sensor < border) { //сход с линии
-        out_of_line();  
-    }
+        search_time++;
+        delay(1);
 
-    delay (15);          
+        if (lastseen > border) {
+            
+            out_of_line_left();  //Сход с линии влево
+
+            if (search_time > 5000) {
+                stop();
+            }
+        }
+
+        else {
+
+            out_of_line_right();  //Сход с линии вправо
+
+            if (search_time > 5000) {
+                stop();
+            }
+        }
+    }        
 }
