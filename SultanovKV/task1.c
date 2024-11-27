9a1e9d8d759029a592d000033497d9b1d748d459
Kirill123456789010 <kir.sultanov2017@gmail.com>
2024-11-27 13:29:44 +0300

    Update task1.c

--- SultanovKV/task1.c
+++ SultanovKV/task1.c
@@ -1,23 +1,24 @@
 #include<stdio.h>
-#include<math.h>
+#include <math.h>
 
-typedef long long int Money;
-const double Inflation = 0.08;                // Инфляция
-const double Deposite = 0.6;                  // Ставка депозита
-const double Indexsation = 0.05;              // Индексация
+typedef long long  Money;
+const double INFLATION = 0.09;                   // Инфляция
+const double INDEXSATION = 0.04;                 // Индексация
+const double DEPOSITE = 0.5;                     // Ставка депозита 
 
-struct Hero {
-	Money salary;                             // Зарплата
-	Money food;                               // Еда
-	Money expense;                            // Вб, Озон, Различные покупки
-	Money transport;                          // Транспорт
-	Money deposit;                            // Депозит
-	Money flat;                               // Квартира
-	Money communal_bills;                     // Комунальные услуги
-	Money monthly_mortgage_pay;               // Месячная Оплата Ипотеки
-	Money loan_amount;                        // Cумма Кредита
-	int annual_rate;                          // Годовая ставка
-	int term;                                 // Срок аренды
+struct Person
+{
+	Money salary;                                     // Зарплата
+	Money deposit;                                    // Депозит
+	Money food;                                       // Еда
+	Money transport;                                  // Транспорт                             
+	Money expense;                                    // Вб, Озон, Различные покупки
+	Money communal_bills;                             // Комунальные услуги
+	Money monthly_mortgage_pay;                       // Месячная Оплата Ипотеки
+	Money loan_amount;                                // Cумма Кредита
+	int annual_rate;                                  // Годовая ставка
+	int term;                                         // Срок аренды 
+
 };
 
 
@@ -25,156 +26,148 @@ struct Cat {
 	int year_of_purchase;
 	int month_of_purchase;
 	Money cost;
-	Money food_cost;
-	Money medical_cost;
-	Money funeral_cost;
-	int duration_of_life;
+	Money cost_of_feed;
+	Money money_for_veterinarian;
+	int count_of_cat;
 };
+ 
 
+Person bob;
+Person alice;
 
-Hero Bob;
-Hero Alice;
+void bob_init()
+{
 
-
-void Bob_init() {
-
-	Bob.salary =  100*1000;
-	Bob.food =   10*1000;
-	Bob.expense = 10*1000;
-	Bob.transport = 6 * 1000;
-	Bob.deposit = 1000 * 10000; 
+	bob.salary = 10 * 10;
+	bob.deposit = 1000 * 1000;
+	bob.food = 20 * 1000;
+	bob.transport = 6 * 1000;
+	bob.expense = bob.food + bob.transport;
+	bob.communal_bills= 5 * 1000;
 }
 
 
-void Bob_flat(const int month) {
+void alice_init()
+{
 
-	Bob.deposit -= Bob.flat;
-	if (month == 12) {
-		Bob.flat *= 1. + Inflation;
-	}
+	alice.salary = 200 * 1000;
+	alice.deposit = 1000 * 1000;
+	alice.food = 25 * 1000;
+	alice.transport = 10 * 1000;
+	alice.expense = alice.food + alice.transport;
+	alice.communal_bills = 4 * 1000;
+	alice.monthly_mortgage_pay;
+	alice.loan_amount = 15 * 1000 * 1000;
+	alice.annual_rate = 16;
+	alice.term = 30;
 }
 
 
-void Bob_salary(const int month) {
+void bob_money(const int month)
+{
+	bob.deposit += bob.salary;
 
-	Bob.deposit += Bob.salary;
 	if (month == 12)
-		Bob.salary *= 1. + Indexsation;
+		bob.salary *= 1. + INDEXSATION;
 }
 
 
-void Bob_expense(const int month) {
+void bob_flat(const int month)
+{
+	bob.deposit -= bob.monthly_mortgage_pay;
 
-	Bob.deposit -= Bob.expense;
 	if (month == 12) {
-		Bob.expense *= 1. + Inflation;
+		bob.monthly_mortgage_pay *= 1. + INFLATION;
 	}
 }
 
 
-
-void Bob_deposite(const int month)
+void bob_deposit(const int month)
 {
 	if (month == 12)
 	{
-		Bob.deposit *= 1. + Deposite;
+		bob.deposit *= 1. + DEPOSITE;
 	}
 }
 
 
-void Alice_init() {
+void bob_expense(const int month)
+{
 
-	Alice.salary = 200 * 1000;
-	Alice.food = 25 * 1000;
-	Alice.transport = 10 * 1000;
-	Alice.deposit = 800* 1000;
-	Alice.monthly_mortgage_pay;
-	Alice.loan_amount = 15 * 1000 * 1000;
-	Alice.annual_rate = 16;
-	Alice.term = 30;
-	Alice.expense = Alice.food + Alice.transport;
-}
-
-
-void Alice_salary(const int month) {
-
-	Alice.deposit += Alice.salary;
-	if (month == 12)
-		Alice.salary *= 1. + Indexsation;
-}
-
-//
-void Alice_cat(const int month, const int year) {
-
-	if ((year > 2030 || (month >= 9 && year = 2029)) && (year < 2045 || (month <= 11 && year = 2045))) {
-
-
-	}
-
-
-
-
-}
-
-
-void Alice_expense(const int month){
-
-	Alice.deposit -= Alice.expense;
+	bob.deposit -= bob.expense;
 	if (month == 12) {
-		Alice.expense *= 1. + Inflation;
+		bob.expense *= 1. + INFLATION;
 	}
 }
 
 
-void Alice_deposite(const int month){
-	
-	if (month == 12)
-	{
-		Alice.deposit *= 1. + Deposite;
-	}
-}
+void alice_communal_bills(const int month)
+{
+	alice.deposit -= alice.communal_bills;
 
-
-void Alice_communal_bills(const int month){
-
-	Alice.deposit -= Alice.communal_bills;    
 	if (month == 12) {
-		Alice.communal_bills *= 1. + Inflation;
+		alice.communal_bills *= 1. + INFLATION;
 	}
 }
 
+void alice_money(const int month)
+{
+	alice.deposit += alice.salary;
 
-void Alice_mortgage_monthly_pay() {
+	if (month == 12)
+		alice.salary *= 1. + INDEXSATION;
+}
 
-	double monthly_rate = Alice.annual_rate / 12.0 / 100.0;
-	int total_month = Alice.term * 12;
+void alice_mortgage_monthly_pay()
+{
+	double monthly_rate = alice.annual_rate / 12.0 / 100.0;
+	int total_month = alice.term * 12;
 
-	Alice.monthly_mortgage_pay = Alice.loan_amount *
+	alice.monthly_mortgage_pay = alice.loan_amount *
 		(monthly_rate * pow(1 + monthly_rate, total_month))
 		/ (pow(1 + monthly_rate, total_month) - 1);
 
-	Alice.deposit -= Alice.monthly_mortgage_pay;
+	alice.deposit -= alice.monthly_mortgage_pay;
 
 }
 
 
-void simulation(int month, int year) {
+void alice_expense(const int month)
+{
+	alice.deposit -= alice.expense;
+	if (month == 12) {
+		alice.expense *= 1. + INFLATION;
+	}
+}
 
+
+void alice_deposit(const int month)
+{
+	if (month == 12)
+	{
+		alice.deposit *= 1. + DEPOSITE;
+	}
+}
+
+
+void simulation(int month, int year)
+{
 	int year_now = year;
 	int month_now = month;
 
 	while (!(year == (year_now + 30) && month == month_now))
 	{
-		Bob_salary(month);
-		Bob_expense(month);
-		Bob_flat(month);
-		Bob_deposite(month);
 
-		Alice_salary(month);
-		Alice_expense(month);
-		Alice_communal_bills(month);
-		Alice_deposite(month);
-		Alice_mortgage_monthly_pay();
+		bob_money(month);
+		bob_expense(month);
+		bob_flat(month);
+		bob_deposit(month);
+
+		alice_money(month);
+		alice_expense(month);
+		alice_communal_bills(month);
+		alice_deposit(month);
+		alice_mortgage_monthly_pay();
 
 		month++;
 
@@ -182,24 +175,27 @@ void simulation(int month, int year) {
 			month = 1;
 			++year;
 		}
+
 	}
-	Alice.deposit += Alice.loan_amount * (1. + Inflation) * Alice.term;
 }
 
 
 void print_result()
 {
-	printf("Bob capital = % lld RUB \n", (Money)(Bob.deposit));
-	printf("Alice capital = % lli RUB\n", (Money)(Alice.deposit));
+	printf("Bob capital = % lld \n", (Money)(bob.deposit));
+	printf("Alice capital = % lld \n", (Money)(alice.deposit));
 }
 
 
 int main()
 {
-	Alice_init();
-	Bob_init();
+	alice_init();
+	bob_init();
+
 	simulation(9, 2024);
+
 	print_result();
 
 	return 0;
+
 }
