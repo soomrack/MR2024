21c815783ca10b2aad6a2acc53ce2b66d90cde30
Tiredelephant18 <147646766+Tiredelephant18@users.noreply.github.com>
2025-01-13 12:16:04 +0300

    Add files via upload

--- Skornyakova/task_2.c
+++ Skornyakova/task_2.c
@@ -39,7 +39,7 @@ void matrix_error(const Status error, char * error_messsage)
 
 Matrix matrix_init(const size_t rows, const size_t cols )
 {  
-     Matrix A = {0, 0, NULL};
+    Matrix A = {0, 0, NULL};
 
     if(rows == 0 || cols == 0) {
         return (Matrix){rows, cols, NULL};
@@ -50,7 +50,7 @@ Matrix matrix_init(const size_t rows, const size_t cols )
        return (Matrix){rows, cols, NULL};
     }
 
-    A.data = (double *)malloc(rows * cols * sizeof(double));// посмотреть на деление, где там sizeof
+    A.data = (double *)malloc(rows * cols * sizeof(double));
     if (A.data == NULL){
         matrix_error(ALLOC_ERR,"Failed to allocate memory for matrix\n");
         return A;
@@ -67,12 +67,13 @@ void matrix_set(const Matrix A, const double *values)
 }
 
 
-void matrix_free(Matrix *A)
+int matrix_free(Matrix *A)
 {
-  if (A->data == NULL) {
-       matrix_error(NULL_ERR, "Incorrect matrix\n");
-    };
-  free(A -> data);
+  if (A == NULL) {
+  matrix_error(NULL_ERR, "Incorrect matrix\n");
+  return -1;
+  }
+  free(A ->data);
   A->data = NULL;
   A->rows = 0;
   A->cols = 0;
@@ -98,21 +99,21 @@ Matrix  matrix_sum(const Matrix A, const Matrix B )
 {
     if (!((A.cols == B.cols) && (A.rows == B.rows))) {
         matrix_error(SIZE_ERR, "Matrix size not equals\n");
+        return EMPTY;
     }
     Matrix C = matrix_init(A.cols, A.rows);
-    for(size_t idx = 0; idx < A.cols * A.rows; idx ++) {
+    for(size_t idx = 0; idx < C.cols * C.rows; idx ++) {
         C.data[idx] = A.data[idx] + B.data[idx];
     }
     matrix_print(C);
     return C;
-
 }
 
 // B = k*A 
 Matrix multiplication_by_a_constant (const Matrix A, double k)
 {
     Matrix B = matrix_init(A.rows, A.cols);
-    for (size_t idx = 0; idx < A.cols * A.rows; idx ++) {
+    for (size_t idx = 0; idx < B.cols * B.rows; idx ++) {
             B.data[idx] = A.data[idx] * k ;
     }
     matrix_print(B);
@@ -121,15 +122,13 @@ Matrix multiplication_by_a_constant (const Matrix A, double k)
 
 //  A += B
 Matrix  matrix_add( Matrix A, const Matrix B )
-{
-   if ((A.cols == B.cols) && (A.rows == B.rows)) {
-        for(size_t idx = 0; idx < A.cols * A.rows; idx ++)
-        {
-            A.data[idx] += B.data[idx];
-        }
+{ 
+   if (!((A.cols == B.cols) && (A.rows == B.rows))) return EMPTY;
+    for(size_t idx = 0; idx < A.cols * A.rows; idx ++) {
+        A.data[idx] += B.data[idx];
+    }
         matrix_print(A);
-        return A; 
-    }  
+        return A;    
 }
 
 //C = A*B
@@ -137,30 +136,35 @@ Matrix matrix_multiply(const Matrix A, const Matrix B)
 { 
     if (!(A.cols == B.rows )){
         matrix_error(SIZE_ERR, "Incorrect matrix size \n");
+        return EMPTY;
     }
     Matrix C = matrix_init(A.rows, B.cols);
-        for(size_t row = 0; row < A.rows; row ++){   
-            for(size_t col = 0; col < B.cols; col ++){
-                C.data[row*C.cols + col]= 0;
+        for(size_t row = 0; row < C.rows; row ++){   
+            for(size_t col = 0; col < C.cols; col ++){
+                C.data[row*C.cols + col]= 0.0;
                 for(size_t idx = 0; idx < A.cols; idx ++){
                     C.data[row * C.cols + col] += A.data[row * A.cols +idx] * B.data[B.cols * idx + col];
                 }
             } 
         }
+
+    
     return C;    
 }
 
 //B = (A)t
 Matrix matrix_transp(const Matrix A)
 {
-    Matrix B = matrix_init(A.cols, A.rows);
-    for (size_t i = 0; i < A.cols; i++ ){
-        for( size_t j = 0; j < A.rows; j++){
-            A.data[i * A.cols + j] = B.data[j*B.cols + i];
+    Matrix B = matrix_init(A.rows, A.cols);
+
+    for (size_t col = 0; col < B.cols; col++ ){
+        for( size_t row = 0; row < B.rows; row++){
+            B.data[row*B.cols + col] = A.data[col * A.cols + row];
         }
     }
-    return B;
     matrix_print(B);
+    return B;
+    
 }
 
 //Определитель
@@ -168,9 +172,11 @@ double matrix_det(const Matrix A)
 {   
     if (!(A.cols == A.rows)) {
        matrix_error(SIZE_ERR, "Incorrect matrix size \n");
+       return NAN;
     }
     if (A.data == NULL) {
        matrix_error(NULL_ERR, "Incorrect matrix\n");
+       return NAN;
     }
     double det = 1;
     double now;
@@ -186,7 +192,6 @@ double matrix_det(const Matrix A)
         }
         det *= -1;
     }
-    
     for (size_t col_0 = 0; col_0 < A.cols-1; col_0++){ 
         for(size_t row_0 = col_0 + 1; row_0 < A.rows; row_0++) {
             double k = A.data[row_0 * A.cols + col_0]/A.data[col_0 * A.cols + col_0];
@@ -205,12 +210,16 @@ double matrix_det(const Matrix A)
 Matrix matrix_copy(const  Matrix A)
 {
     Matrix copy = matrix_init(A.cols, A.rows);
-    memcpy(copy.data,A.data, A.cols * A.rows * sizeof(double));
+    if (A.data == NULL) {
+       matrix_error(NULL_ERR, "Incorrect matrix\n");
+       return EMPTY;
+    }
+    memcpy(copy.data, A.data, A.cols * A.rows * sizeof(double));
     return copy;
 }
 
 //факториал 
-double factorial(const  int k)
+double factorial(const unsigned int k)
 {
     double factorial = 1.0;
 
@@ -221,21 +230,20 @@ double factorial(const  int k)
     for (size_t i = 1; i <= k; ++i) {
        factorial *= i;
     }
-    printf("%f", factorial);
     return factorial;
 }
 
 // B -Единичная матрица размерностью A
-Matrix identity_matrix(Matrix A)
+Matrix matrix_identity(Matrix A)
 {
     Matrix B = matrix_init(A.cols, A.rows);
-    for (size_t i=0; i<A.rows; i++){
-        for (size_t j=0; j<A.cols; j++){
-            if (i==j){
-                B.data[i * A.cols + i] = 1;
+    for (size_t row = 0; row <B.rows; row++){
+        for (size_t col=0; col <B.cols; col++){
+            if (row == col){
+                B.data[row * A.cols + row] = 1;
             }
             else {
-                B.data[i * A.cols + j] = 0;
+                B.data[row * A.cols + col] = 0;
             }
         }
     }
@@ -243,15 +251,15 @@ Matrix identity_matrix(Matrix A)
 }
 
 // Возведение матрицы в степень
-Matrix matrix_pow( Matrix A, int pow) 
+Matrix matrix_pow(const  Matrix A, const unsigned int pow)  
 {
     if (A.rows != A.cols) {
         matrix_error(SIZE_ERR, "Incorrect matrix size \n");
-    
+        return EMPTY;
     }
 
     if (pow == 0) {
-        Matrix result = identity_matrix(A);
+        Matrix result = matrix_identity(A);
         return result;
     }
 
@@ -261,10 +269,9 @@ Matrix matrix_pow( Matrix A, int pow)
     for (int i = 1; i < pow; ++i) {
         result = matrix_multiply(matrix_to_power, A);
         matrix_free(&matrix_to_power);
-        matrix_to_power =  matrix_copy(result); 
+        matrix_to_power = matrix_copy(result); 
         
     }
-
     matrix_free(&result);
     return matrix_to_power;
 }
@@ -274,32 +281,37 @@ Matrix matrix_exp(const  Matrix A, int k)
 {
     if (A.rows != A.cols) {
         matrix_error(SIZE_ERR, "Incorrect matrix size \n");
+        return EMPTY;
     }
 
     Matrix result = matrix_init(A.rows, A.cols);
+    //printf("%d,%d\n",A.rows, A.cols);
+   // printf("%d,%d\n",result.rows, result.cols);
     Matrix matrix_to_power = matrix_init(A.rows, A.cols);
-    Matrix E = identity_matrix(A); 
-
+    
     for (int n = 0; n < k; ++n) {
         matrix_to_power = matrix_pow(A, n);
         double fact = factorial(n);
 
-        for (size_t i = 0; i < result.rows * result.cols; ++i) {
-            result.data[i] += (matrix_to_power.data[i] / fact + E.data[i]) ;
+        for (size_t idx = 0; idx < result.rows * result.cols; idx++) {
+            result.data[idx] += matrix_to_power.data[idx] / fact;
         }
-
         matrix_free(&matrix_to_power);
+        //matrix_print(result);
     }
-
+    matrix_free(&matrix_to_power);
+    //matrix_print(result);
     return result;
 }
 
 int main()
 {
-   Matrix A, B,C,M;
+   Matrix A, B,C,M,D;
     A = matrix_init(3, 3);  
     B = matrix_init(3, 3);  
     C = matrix_init(2, 2);
+    M = matrix_init(3, 3);
+    D = matrix_init(4, 4);
     matrix_set(A, (double[]){
             3., 4., 5., 
             7., 8., 9., 
@@ -314,16 +326,31 @@ int main()
             1., 0.,  
             0., 1.
         });
-    matrix_print(A);
-    matrix_print(B);
-    //C = matrix_multiply(B,B);
+            matrix_set(D, (double[]){
+            1., 0., 1., 1.,
+            0., 1., 1.,1.,
+            0., 1., 1.,1.,
+            1., 1., 1.,1.
+        });
+    //matrix_print(A);
+    //matrix_print(B);
+    //matrix_print(C);
+    //C = matrix_sum(B,B);
+    //C = matrix_multiply(A,B);
     //C = matrix_pow(B,2);
     //C = identity_matrix(A);
-    M = matrix_exp(C, 2);
+   // printf("%d,%d\n",M.rows, M.cols);
+
+    M = matrix_exp(B, 20);
+    //matrix_print(C);
+    //C = matrix_transp(A);
+    //C = matrix_sum(A,B);
     matrix_print(M);
+    //factorial(20);
+    //matrix_print(C);
     
     
     //double det = matrix_det(A);
     //printf("%f",det);
-    
+
 }
