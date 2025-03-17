445f2815e0512f2545ff7de7cece79865c72adf9
Egor842 <75377311+Egor842@users.noreply.github.com>
2025-03-17 23:36:06 +0300

    Add files via upload

---
+++ Meshkov_Egor/greenhouse/aero/airlines_list.cpp
@@ -0,0 +1,134 @@
+#include "airlines_list.hpp"
+#include "rapidcsv.h"
+#include <algorithm>
+#include <climits>
+#include <string>
+#include <exception>
+#include <queue>
+#include "opencv4/opencv2/opencv.hpp"
+#include "opencv4/opencv2/core/mat.hpp"
+
+GL::Airlines_list& GL::Airlines_list::getInstance() noexcept {
+    static Airlines_list instance;
+    cv::Mat mat;
+    return instance;
+}
+
+
+void GL::Airlines_list::load_data(const std::string& csv_file) {
+    if (!graph.empty()) this->clear();
+
+    try {
+        rapidcsv::Document doc(csv_file);
+
+        for (size_t i = 0; i < doc.GetRowCount(); ++i) {
+            FlightInfo info;
+
+            info.air_time = doc.GetCell<double>("AIR_TIME", i);
+            info.origin_airport_id = doc.GetCell<int>("ORIGIN_AIRPORT_ID", i);
+            info.dest_airport_id = doc.GetCell<int>("DEST_AIRPORT_ID", i);
+            info.year = doc.GetCell<int>("YEAR", i);
+            info.quarter = doc.GetCell<int>("QUARTER", i);
+            info.month = doc.GetCell<int>("MONTH", i);
+
+            airports.insert(info.origin_airport_id);
+            airports.insert(info.dest_airport_id);
+
+            graph[info.origin_airport_id].emplace_back(info.dest_airport_id, info);
+        }
+    } catch (const std::exception& e) {
+        throw Airlines_listException("Error of read CSV-file: " + std::string(e.what()));
+    }
+}
+
+
+void GL::Airlines_list::clear() noexcept {
+    graph.clear();
+    airports.clear();
+}
+
+
+bool GL::Airlines_list::contains_airport(int index) const noexcept {
+    return (airports.find(index) != airports.end()); 
+}
+
+
+GL::FlightPath GL::Airlines_list::find_flight_path_between(int origin_index, int dest_index) const {
+    if(!this->contains_airport(origin_index)) {
+        throw Airlines_listException("Origin airport index not found: " + std::to_string(origin_index));
+    }
+
+    if(!this->contains_airport(dest_index)) {
+        throw Airlines_listException("Dest airport index not found: " + std::to_string(dest_index));
+    }
+
+    FlightPathRow path = dijkstra(origin_index);
+
+    return reconstruct_path(path, dest_index);
+}
+
+
+GL::Airlines_list::FlightPathRow GL::Airlines_list::dijkstra(int origin_index) const noexcept {
+    std::unordered_map<int, double> distance;
+    std::unordered_map<int, int> parents;
+
+    for (int airport : airports) {
+        distance[airport] = std::numeric_limits<double>::max();
+        parents[airport] = -1;
+    }
+
+    distance[origin_index] = 0;
+
+    using Pair = std::pair<double, int>; // {distance, vertex}
+    auto cmp = [](const Pair& a, const Pair& b) { return a.first > b.first; };
+    std::priority_queue<Pair, std::vector<Pair>, decltype(cmp)> q(cmp);
+
+    q.push({0, origin_index});
+
+    while (!q.empty()) {
+        auto [curr_distance, curr_dest] = q.top();
+        q.pop();
+
+        if (curr_distance > distance[curr_dest]) continue;
+        if (graph.find(curr_dest) == graph.end()) continue;
+
+        for (const auto& [dest_vertex, edge] : graph.at(curr_dest)) {
+            if (distance[dest_vertex] > distance[curr_dest] + edge.air_time) {
+                distance[dest_vertex] = distance[curr_dest] + edge.air_time;
+                parents[dest_vertex] = curr_dest;
+                q.push({distance[dest_vertex], dest_vertex});
+            }
+        }
+    }
+
+    return {distance, parents};
+}
+
+
+GL::FlightPath GL::Airlines_list::reconstruct_path(GL::Airlines_list::FlightPathRow& path, int dest_index) const noexcept {
+    std::vector<int> reconstruct_parents;
+    std::vector<double> reconstruct_distances;
+
+    for(int curr_index = dest_index; curr_index != -1; curr_index = path.second[curr_index]) {
+        reconstruct_parents.push_back(curr_index);
+    }
+
+    std::reverse(reconstruct_parents.begin(), reconstruct_parents.end());
+
+    for(size_t i = 1; i < reconstruct_parents.size(); ++i) {
+        int prev_index = reconstruct_parents[i - 1];
+        int curr_index = reconstruct_parents[i];
+
+        if (graph.find(prev_index) == graph.end()) continue;
+
+        for (const auto& [dest_vertex, edge] : graph.at(prev_index)) {
+            if (dest_vertex == curr_index) {
+                reconstruct_distances.push_back(edge.air_time);
+                break;
+            }
+        }
+    }
+
+    return {reconstruct_parents, reconstruct_distances};
+}
+
---
+++ Meshkov_Egor/greenhouse/aero/airlines_list.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#include <cstdlib>
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+
+namespace GL {
+
+
+class Airlines_listException {
+private:
+    std::string msg_err;
+public:
+    explicit Airlines_listException(const std::string& msg) : msg_err(msg) {} 
+    
+    const char* what() const noexcept {
+        return msg_err.c_str();
+    }
+};
+
+
+using FlightPath = std::pair<std::vector<int>, std::vector<double>>; // parents && distance
+
+
+class Airlines_list {
+private:
+    struct FlightInfo {
+        double air_time;
+        int origin_airport_id;
+        int dest_airport_id;
+        int year;
+        int quarter;
+        int month;
+    };
+
+    using Graph = std::map<int, std::vector<std::pair<int, FlightInfo>>>;
+    using Airports = std::unordered_set<int>;
+
+    Graph graph;
+    Airports airports;
+public:
+    static Airlines_list& getInstance() noexcept;
+    void load_data(const std::string& file_path);
+    void clear() noexcept;
+    bool contains_airport(int index) const noexcept;
+    FlightPath find_flight_path_between(int origin_index, int dest_index) const;
+    
+    using FlightPathRow = std::pair<std::unordered_map<int, double>, std::unordered_map<int, int>>;
+private:
+    Airlines_list() : graph(), airports() {}
+    Airlines_list(const Airlines_list&) = delete;
+    Airlines_list(Airlines_list&&) = delete;
+    ~Airlines_list() = default;
+
+    FlightPathRow dijkstra(int origin_index) const noexcept;
+    FlightPath reconstruct_path(FlightPathRow& path, int dest_index) const noexcept;
+};
+
+
+};
