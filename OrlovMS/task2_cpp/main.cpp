b10cd39e744df0afaf684491d11dda87149ee48b
Михаил Орлов <78139868+orlovms22@users.noreply.github.com>
2025-03-18 03:19:07 +0300

    Exception in constructor test

--- OrlovMS/task2_cpp/main.cpp
+++ OrlovMS/task2_cpp/main.cpp
@@ -3,5 +3,16 @@
 
 int main()
 {
+    Matrix::set_log_level(Matrix::LOG_INFO);
+    try
+    {
+        Matrix mat(SIZE_MAX, SIZE_MAX);
+        mat.set_identity();
+        mat.print();
+    }
+    catch(const MatrixException& err)
+    {
+        std::cout << err.get_code() << " " << err.what() << std::endl;
+    }
     return 0;
 }
