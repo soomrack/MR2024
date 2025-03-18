a8d5b93d0d9447c28fe4cfef5651566b7e1c7afe
Maksikanchik <147877250+Maksikanchik@users.noreply.github.com>
2025-03-18 12:27:33 +0300

    Update task5.ino

--- Aksenov Maksim/task5.ino
+++ Aksenov Maksim/task5.ino
@@ -11,16 +11,26 @@ DHT11 dht11(AIR_PIN);
 #define WATER_PUMP_PIN 5
 #define LED_LIGHT_PIN 6
 #define FAN_PIN 7
-#define HEAT_FAN_PIN 4
+#define HEAT_PIN 4
 
 
 struct Climate 
 {
-  int desired_temp;
-  int desired_soil_humi;
-  int desired_air_humi;
-  int desired_light;
+  int temp_min;
+  int temp_max;
+
+  int soil_humi_min;
+  int soil_humi_max;
+  
+  int air_humi_min;
+  int air_humi_max;
+  
+  int light_min;
+  int light_max;
+  
   int pump_work_time;
+  int pump_dead_time;
+  
   int light_duration;
   int ventilation_duration;
   int sleep_start;
@@ -30,13 +40,24 @@ struct Climate
 
 Climate currentClimate = // Default climate
 {
-  250,   // TEMP
-  550,   // SOIL_HUMI
-  30,    // AIR_HUMI
-  700,   // LIGHT
-  1/20 * 60 * 1000, // Pump 3 sec
-  1/60 * 60 * 1000, // Light 1 sec
-  1 * 60 * 1000,  // Vent 1 min
+  240, // TEMP_MIN
+  260, // TEMP_MAX
+  
+  550, // SOIL_HUMI_MIN
+  540, // SOIL_HUMI_MAX
+  
+  20, // AIR_HUMI_MIN
+  30, // AIR_HUMI_MAX
+  
+  600,   // LIGHT_MIN
+  700, // LIGHT_MAX
+  
+  1/3 * 60 * 1000, // Pump work min 20 sec
+  1/2 * 60 * 1000, // Pump dead 30 sec
+  
+  1/60 * 60 * 1000, // Light min 1 sec
+  1 * 60 * 1000,  // Vent min 1 minute
+
   20, // Sleep in (hours)
   8 // Wake up in (hours)
 };
@@ -44,11 +65,20 @@ Climate currentClimate = // Default climate
 
 Climate tomatoes = 
 {
-  360,   // TEMP
-  550,   // SOIL_HUMI
-  10,    // AIR_HUMI
-  700,   // LIGHT
+  320,   // TEMP_MIN
+  340, // TEMP_MAX
+  
+  450,   // SOIL_HUMI_MIN
+  420, // SOIL_HUMI_MAX
+  
+  15,    // AIR_HUMI_MIN
+  20, // AIR_HUMI_MAX
+  
+  650,   // LIGHT_MIN
+  710, //LIGHT_MAX
+
   2 * 60 * 1000, // Pump 2 min
+  5 * 60 * 1000, // Pump dead 5 min
   5 * 60 * 1000, // Light 5 min
   3 * 60 * 1000,  // Vent 3 min
   21, // Sleep in (hours)
@@ -58,11 +88,20 @@ Climate tomatoes =
 
 Climate cucumbers = 
 {
-  340,   // TEMP
-  500,   // SOIL_HUMI
-  15,    // AIR_HUMI
-  650,   // LIGHT
-  2 * 60 * 1000,  // Punp 2 min
+  280,   // TEMP_MIN
+  300, // TEMP_MAX
+  
+  580,   // SOIL_HUMI_MIN
+  550, // SOIL_HUMI_MAX
+  
+  10,    // AIR_HUMI_MIN
+  15, // AIR_HUMI_MAX
+  
+  600,   // LIGHT_MIN
+  700, //LIGHT_MAX
+
+  2 * 60 * 1000,  // Pump 2 min
+  5 * 60 * 1000, // Pump dead 5 min
   7 * 60 * 1000,  // Light 7 min
   10 * 60 * 1000,   // Vent 10 min
   19, // Sleep in (hours)
@@ -72,9 +111,9 @@ Climate cucumbers =
 
 typedef unsigned long int time;
 
+time cur_millis;
 unsigned int cur_hour, cur_min, cur_sec;
 unsigned int ext_light, soil_humi, air_humi, air_temp;
-bool is_heating, is_airing;
 bool allow_heating = 1;
 
 
@@ -86,7 +125,7 @@ void pin_init()
   pinMode(WATER_PUMP_PIN, OUTPUT);
   pinMode(LED_LIGHT_PIN, OUTPUT);
   pinMode(FAN_PIN, OUTPUT);
-  pinMode(HEAT_FAN_PIN, OUTPUT);
+  pinMode(HEAT_PIN, OUTPUT);
 }
 
 
@@ -100,12 +139,12 @@ void RTC_set(unsigned int hour_to_set, unsigned int min_to_set, unsigned int sec
 
 void RTC_count()
 {
-  static time last_millis = millis();
+  static time last_millis = cur_millis;
 
-  if ((millis() - last_millis) >= 1000)
+  if ((cur_millis - last_millis) >= 1000)
   {
-    cur_sec = cur_sec + (millis() - last_millis) / 1000;
-    last_millis = millis();
+    cur_sec = cur_sec + (cur_millis - last_millis) / 1000;
+    last_millis = cur_millis;
   }
   
   if (cur_sec >= 60)
@@ -129,7 +168,7 @@ void RTC_count()
 
 void RTC_print()
 {
-  Serial.print("------------------------------\n");
+  Serial.println("------------------------------");
   Serial.println("Time: " + String(cur_hour) + ":" + String(cur_min) + ":" + String(cur_sec));
 }
 
@@ -146,15 +185,6 @@ void off(int PIN)
 }
 
 
-void get_info() 
-{
-  ext_light = analogRead(EXTERNAL_LIGHT_PIN);
-  soil_humi = analogRead(SOIL_HUMI_PIN);
-  air_humi = dht11.readHumidity();
-  air_temp = dht11.readTemperature();
-}
-
-
 void lighting_check()
 {
   static time t_light;
@@ -166,12 +196,12 @@ void lighting_check()
     return;
   }
 
-  if ((ext_light >= currentClimate.desired_light) && (digitalRead(LED_LIGHT_PIN) == 0)) {
+  if ((ext_light >= currentClimate.light_min) && (digitalRead(LED_LIGHT_PIN) == 0)) {
     on(LED_LIGHT_PIN);
-    t_light = millis();
+    t_light = cur_millis;
   }
 
-  if ((millis() - t_light >= currentClimate.light_duration) && (ext_light < currentClimate.desired_light)) {
+  if ((cur_millis - t_light >= currentClimate.light_duration) && (ext_light <= currentClimate.light_max)) {
     off(LED_LIGHT_PIN);
   }
 }
@@ -180,78 +210,68 @@ void lighting_check()
 void watering_check() 
 {
   static time p_time;
+  static time dead_time_start;
 
-  if ((soil_humi >= currentClimate.desired_soil_humi) && (digitalRead(WATER_PUMP_PIN) == 0)) {
+  if ((soil_humi >= currentClimate.soil_humi_min) && (digitalRead(WATER_PUMP_PIN) == 0) && (cur_millis - dead_time_start >= currentClimate.pump_dead_time)) {
     allow_heating = 0;
     on(WATER_PUMP_PIN);
-    p_time = millis();
+    p_time = cur_millis;
   }
 
-  if ((millis() - p_time >= currentClimate.pump_work_time) && (soil_humi < currentClimate.desired_soil_humi)) {
+  if ((digitalRead(WATER_PUMP_PIN) == 1) && (cur_millis - p_time >= currentClimate.pump_work_time) && (soil_humi <= currentClimate.soil_humi_max)) {
     off(WATER_PUMP_PIN);
     allow_heating = 1;
-  }
-}
-
-
-void airing_check() 
-{
-  if (air_humi >= currentClimate.desired_air_humi) {
-    is_airing = 1;
-  } else {
-    is_airing = 0;
+    dead_time_start = cur_millis;
   }
 }
 
 
 void heating_check() 
-{ 
-  if (air_temp <= currentClimate.desired_temp) {
-    is_heating = 1;
-  } else {
-    is_heating = 0;
-  }
-}
-
-
-void fan_check() 
 {
-  static time t_fan;
+  static time t_heat;
 
-  airing_check();
-  heating_check();
-
-  if (is_heating || is_airing) {
-    if (is_heating && allow_heating) {
-      on(HEAT_FAN_PIN);
-      //Serial.println("HEAT ON"); // For debugging
-    } else {
-      off(HEAT_FAN_PIN);
-      //Serial.println("HEAT OFF"); // For debugging
-    }
-
-    if (digitalRead(FAN_PIN) == 0) {
-      on(FAN_PIN);
-      t_fan = millis();
-    }
+  if (!allow_heating) {
+    off(FAN_PIN);
+    off(HEAT_PIN);
+    //Serial.println("HEAT OFF"); // For debugging
+    return;
   }
 
-  if (millis() - t_fan >= currentClimate.ventilation_duration) {
+  if ((air_temp <= currentClimate.temp_min) && (digitalRead(FAN_PIN) == 0)) {
+    on(HEAT_PIN);
+    on(FAN_PIN);
+    t_heat = cur_millis;
+    //Serial.println("HEAT ON"); // For debugging
+  }
+
+  if ((cur_millis - t_heat >= currentClimate.ventilation_duration) && (air_temp >= currentClimate.temp_max)) {
     off(FAN_PIN);
-    off(HEAT_FAN_PIN);
+    off(HEAT_PIN);
   }
 }
 
-void pump_temp_check()
+
+void airing_check()
+{
+  static time t_air;
+
+  if ((air_humi >= currentClimate.air_humi_max) && (digitalRead(FAN_PIN) == 0)) {
+    on(FAN_PIN);
+    t_air = cur_millis;
+  }
+
+  if ((cur_millis - t_air >= currentClimate.ventilation_duration) && (air_humi <= currentClimate.air_humi_min)) {
+    off(FAN_PIN);
+  }
+}
+
+void pump_temp_print()
 {
   Serial.println("------------------------------");
-  Serial.println("Status temp: " + String(digitalRead(HEAT_FAN_PIN)));
+  Serial.println("Status temp: " + String(digitalRead(HEAT_PIN)));
   Serial.println("Status pump: " + String(digitalRead(WATER_PUMP_PIN)));
 
   Serial.println("Allow heating: " + String(allow_heating));
-  Serial.println("Is airing: " + String(is_airing));
-  Serial.println("IS heating: " + String(is_heating));
-
 }
 
 void sensor_print() 
@@ -276,18 +296,26 @@ void setup()
 
 void loop() 
 {
-  get_info();
+  //Sensors_info
+  ext_light = analogRead(EXTERNAL_LIGHT_PIN);
+  soil_humi = analogRead(SOIL_HUMI_PIN);
+  air_humi = dht11.readHumidity();
+  air_temp = dht11.readTemperature();
+  cur_millis = millis();
+  
   RTC_count();
 
   // For debugging 
   RTC_print();
   sensor_print();
-  pump_temp_check();
+  pump_temp_print();
   //
 
+  //Actuators_actions
   lighting_check();
   watering_check();
-  fan_check();
+  airing_check();
+  heating_check();
 
   delay(1000);
 }
