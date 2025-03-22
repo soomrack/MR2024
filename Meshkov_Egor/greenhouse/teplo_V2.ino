b117d35c5b90974223c3bbff99fca49ccb74aa1a
bobvarga <57838733+bobvarga@users.noreply.github.com>
2025-03-22 22:05:27 +0300

    Update teplo_V2.ino

--- Meshkov_Egor/greenhouse/teplo_V2.ino
+++ Meshkov_Egor/greenhouse/teplo_V2.ino
@@ -1,23 +1,63 @@
-#include <DHT11.h>
-#include "Microclimate.hpp"
+#include "Microclimat.hpp"
+#include "DHT11.h"
 
 
-MicroclimateController& controller = MicroclimateController::get_instance();
+#define PIN_LIGHT_SENSOR 13
+#define PIN_HYGROMETER A1
+#define PIN_DHT_SENSOR 9
 
 
-void setup() {
-    controller.set_time_day_start(7);
-    controller.set_time_day_end(20);
-    controller.set_target_soil_humidity_level(45);
-    controller.set_target_temp_level(25);
-    controller.set_target_air_humidity_level(35);
-    controller.set_air_out_time(15, 30, 16, 0);
+#define PIN_BACKLIGHT 6
+#define PIN_PUMP 5
+#define PIN_HEATER 4
+#define PIN_VENTILATION 7
 
-    Serial.begin(115200);
+
+static Climate climate; 
+static DateTime date_time(15, 30); 
+static DHT11 dht(PIN_DHT_SENSOR);
+
+void pins_init() {
+    pinMode(PIN_LIGHT_SENSOR, INPUT);
+    pinMode(PIN_HYGROMETER, INPUT);
+
+    pinMode(PIN_BACKLIGHT, OUTPUT);
+    pinMode(PIN_PUMP, OUTPUT);
+    pinMode(PIN_HEATER, OUTPUT);
+    pinMode(PIN_VENTILATION, OUTPUT);
 }
 
 
+void set_target_params(Climate& climate) {
+    climate.target_lighting_level = 50; 
+    climate.target_soil_humidity_level = 40; 
+    climate.target_air_humidity_level = 30; 
+    climate.target_temp_level = 30; 
+}
+
+
+void setup() {
+    pins_init();
+    set_target_params(climate);
+    Serial.begin(9600);
+}
+ 
+
 void loop() {
-    controller.update();
-    controller.print_current_environment_params();
+    static Backlight backlight(PIN_BACKLIGHT, PIN_LIGHT_SENSOR); 
+    static Pump pump(PIN_PUMP, PIN_HYGROMETER); 
+    static Heater heater(PIN_HEATER, dht); 
+    static Ventilator ventilator(PIN_VENTILATION, dht); 
+
+    control_temperature(climate, heater, ventilator, pump); 
+    control_soil_humidity(climate, pump, heater);
+    control_humidity_air(climate, ventilator);
+    control_lighting(climate, date_time, backlight); 
+
+    do_device(heater);
+    do_device(pump);
+    do_device(ventilator);
+    do_device(backlight);
+
+    print_enviroment_params(date_time, ventilator, backlight, heater, pump);
 }
