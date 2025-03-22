ef48557c422f8e33df019f5a566b17fb3f40d936
bobvarga <57838733+bobvarga@users.noreply.github.com>
2025-03-22 22:05:54 +0300

    Update Microclimate.hpp

--- Meshkov_Egor/greenhouse/Microclimate.hpp
+++ Meshkov_Egor/greenhouse/Microclimate.hpp
@@ -1,72 +1,147 @@
-#ifndef MICROCLIMATE_CONTROLLER_H
-#define MICROCLIMATE_CONTROLLER_H
-
+#pragma once
 
 #include <DHT11.h>
-//#include <cstdint>
 
 
-#define PIN_LIGHT_SENSOR 9
-#define PIN_SOIL_HUMIDITY_SENSOR A1
-#define PIN_DHT_SENSOR 12
-
-
-#define PIN_BACKLIGHT 6
-#define PIN_PUMP 5
-#define PIN_HEATER 4
-#define PIN_VENTILATION 7
-
-
-class MicroclimateController {
-private:
-    MicroclimateController();
-    MicroclimateController(const MicroclimateController&) = delete;
-    MicroclimateController& operator=(const MicroclimateController&) = delete;
-
-    DHT11 dht;
-    
-    bool lighting_is_active = false;
-    bool heater_is_active = false;
-    bool pump_is_active = false;
-    bool ventilation_is_active = false;
-    bool is_set_plan_air_out = false;
-
-    uint16_t air_out_start_time = 0; // In minutes
-    uint16_t air_out_end_time = 0; // In minutes
-
-    uint8_t current_lighting_level = 0;
-    uint8_t current_soil_humidity = 0;
-    int current_temperature = 0;
-    uint8_t current_air_humidity = 0;
-
-    uint8_t time_day_start = 7; // hours
-    uint8_t time_day_end = 23; // hours
+struct Climate {
+    uint8_t target_lighting_level = 50; // % 
     uint8_t target_soil_humidity_level = 40; // %
-    int target_temp_level = 26; // °C
     uint8_t target_air_humidity_level = 30; // %
-
-    unsigned long last_pump_toggle_time = 0;
-    unsigned long last_heater_toggle_time = 0;
-
-    uint16_t get_current_time() const; // In minutes
-    void read_sensor_data();
-    void evaluate_conditions(unsigned long deadtime);
-    void control_devices();
-public:
-    static MicroclimateController& get_instance() {
-        static MicroclimateController instance;
-        return instance;
-    }
-
-    void set_time_day_start(uint8_t start);
-    void set_time_day_end(uint8_t end);
-    void set_target_soil_humidity_level(uint8_t level);
-    void set_target_temp_level(int level);
-    void set_target_air_humidity_level(uint8_t level);
-    void set_air_out_time(uint8_t start_hour, uint8_t start_minute, uint8_t end_hour, uint8_t end_minute);
-    void update(unsigned long deadtime = 1000);
-    void print_current_environment_params() const;
+    int target_temp_level = 30; // °C
 };
 
 
-#endif // MICROCLIMATE_CONTROLLER_H
+class DateTime {
+private:
+    uint16_t time_start_day;
+    uint16_t time_end_day;
+public:
+    explicit DateTime(uint16_t time_start_day, uint16_t time_end_day) noexcept 
+        : time_start_day(time_start_day), time_end_day(time_end_day) {}
+    ~DateTime() = default;
+    
+    // In minutes
+    uint16_t get_time_start_day() const noexcept { return time_start_day; }
+    uint16_t get_time_end_day() const noexcept { return time_end_day; }
+    uint16_t get_current_time() const noexcept;
+private:
+    DateTime() = delete;
+    DateTime(const DateTime&) = delete;
+    DateTime(DateTime&&) = delete;
+};
+
+
+class Backlight {
+private:
+    uint8_t PIN_LIGHT_SENSOR; 
+    uint8_t PIN_BACKLIGHT;
+public: 
+    bool is_need_to_light = false;
+public:
+    explicit Backlight(uint8_t pin_light_sensor, uint8_t pin_backlight)
+        : PIN_LIGHT_SENSOR(pin_light_sensor), PIN_BACKLIGHT(pin_backlight) {}
+    ~Backlight() = default;
+    
+    void turn_on() const noexcept { digitalWrite(PIN_BACKLIGHT, HIGH); }
+    void turn_off() const noexcept { digitalWrite(PIN_BACKLIGHT, LOW); }    
+    uint8_t get_lighting_level() const noexcept { return digitalRead(PIN_BACKLIGHT); }
+private:
+    Backlight() = delete;
+    Backlight(const Backlight&) = delete;
+    Backlight(Backlight&&) = delete;
+};
+
+
+class Pump {
+private:
+    const uint8_t PIN_PUMP;
+    const uint8_t PIN_HYGROMETER;
+public:
+    bool is_active = false;
+public:
+    explicit Pump(uint8_t pin_pump, uint8_t pin_hygrometer) noexcept
+        : PIN_PUMP(pin_pump), PIN_HYGROMETER(pin_hygrometer) {}
+    ~Pump() = default;
+
+    void turn_on() const noexcept { digitalWrite(PIN_PUMP, HIGH); }
+    void turn_off() const noexcept { digitalWrite(PIN_PUMP, LOW); }
+    int get_soil_humidity() const noexcept { return map(analogRead(PIN_HYGROMETER), 0, 1023, 100, 0); }
+private:
+    Pump() = delete;
+    Pump(const Pump&) = delete;
+    Pump(Pump&&) = delete;
+};
+
+
+class Heater {
+private:
+    const uint8_t PIN_HEATER;
+    DHT11& dht;
+public:
+    bool is_active = false;
+public:
+    explicit Heater(uint8_t pin_heater, DHT11& dht) noexcept
+        : PIN_HEATER(pin_heater), dht(dht) {}
+    ~Heater() = default;
+
+    void turn_on() const noexcept { digitalWrite(PIN_HEATER, HIGH); }
+    void turn_off() const noexcept { digitalWrite(PIN_HEATER, LOW); }
+    int get_temperature() const noexcept { return dht.readTemperature(); }
+private:
+    Heater() = delete;
+    Heater(const Heater&) = delete;
+    Heater(Heater&&) = delete;
+};
+
+
+class Ventilator {
+private:
+    const uint8_t PIN_VENTILATOR;
+    DHT11& dht;
+    uint16_t time_start_air_out;
+    uint16_t time_end_day_air_out;
+public:
+    bool is_need_to_heater = false;
+    bool is_need_to_plan_air_out = false;
+    bool is_need_to_control_air_humidity = false;
+public:
+    explicit Ventilator(uint8_t pin_ventilator, DHT11& dht) noexcept 
+        : PIN_VENTILATOR(pin_ventilator), dht(dht) {}
+    ~Ventilator() = default;
+    
+    void turn_on() const noexcept { digitalWrite(PIN_VENTILATOR, HIGH); }
+    void turn_off() const noexcept { digitalWrite(PIN_VENTILATOR, LOW); }
+    uint8_t get_air_humidity() const noexcept { return dht.readHumidity(); }
+    void set_plan_air_out(uint8_t start_hour, uint8_t start_minut, uint8_t end_hour, uint8_t end_minut) noexcept;
+private:
+    Ventilator() = delete;
+    Ventilator(const Ventilator&) = delete;
+    Ventilator(Ventilator&&) = delete;
+};
+
+
+void control_temperature(const Climate& climate, Heater& heater, Ventilator& ventilator, Pump& pump); 
+
+
+void control_soil_humidity(const Climate& climate, Pump& pump, Heater& heater);
+
+
+void control_humidity_air(const Climate& climate, Ventilator& ventilator);
+
+
+void control_lighting(const Climate& climate, DateTime& data_time, Backlight& backlight); 
+
+
+void do_device(const Heater& heater);
+
+
+void do_device(const Pump& pump);
+
+
+void do_device(const Ventilator& ventilator);
+
+
+void do_device(const Backlight& backlight);
+
+
+void print_enviroment_params(DateTime& date_time, Ventilator& ventilator, Backlight& backlight, Heater& heater, Pump& pump);
