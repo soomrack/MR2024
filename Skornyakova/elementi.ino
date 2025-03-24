aa6bece93683bede5eae25fb5230ca7e28de4265
Tiredelephant18 <147646766+Tiredelephant18@users.noreply.github.com>
2025-03-24 13:56:29 +0300

    Add files via upload

--- Skornyakova/elementi.ino
+++ Skornyakova/elementi.ino
@@ -1,15 +1,15 @@
 #include <DHT.h>
 #define DHTTYPE DHT11
 
-#define DHTPIN 12 //переписать все дифы на капс 
-#define LAMP_PIN 6 // лампа
-#define FAN_PIN 7 // вентилятор
-#define NAGREV_PIN 4 // наргреватель
-#define POMP_PIN 5 // помпа
-#define HUM_SOIL_PIN A1 // влажность почвы
-#define ILLUMINATION_PIN 9 // датчик света
+#define DHT_PIN 12        //переписать все дифы на капс 
+#define LAMP_PIN 6        // лампа
+#define FAN_PIN 7         // вентилятор
+#define NAGREV_PIN 4      // наргреватель
+#define PUMP_PIN 5        // помпа
+#define HUM_SOIL_PIN A1     // влажность почвы
+#define ILLUMINATION_PIN A3       // датчик света
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHT_PIN, DHTTYPE);
 
  
 class Climate {
@@ -47,30 +47,28 @@ void set_climate_potato(Climate &climate)
 }
 
 
-boolean FLAG_POMP_SWITCH = false;
-boolean FLAG_POMP_TIME = true;
+boolean FLAG_PUMP_SWITCH = false;
+boolean FLAG_NAGREV_ON = false;
 
  
 class Time {
     private:
-        int hours;
+        
         int minutes;
         int seconds;
         unsigned long previousMillis;
 
     public:
+        int hours;
         int time_of_day;// Время суток (0 - ночь, 1 - день)
         
     public:
-        Time(int h, int m, int s) : hours(h), minutes(m), seconds(s), time_of_day(0), previousMillis(0) {
-            if (hours < 0 || hours > 23) hours = 0;
-            if (minutes < 0 || minutes > 59) minutes = 0;
-            if (seconds < 0 || seconds > 59) seconds = 0;
-            update_time_of_day(); 
-        };
+        Time(int h, int m, int s);
+        void update_time();
+        void update_time_of_day(); 
+};
 
-    public:
-        void update_time() {
+void Time::update_time() {
             const unsigned long interval = 1000; // Интервал в миллисекундах (1 секунда)
             unsigned long currentMillis = millis();
             if (currentMillis - previousMillis >= interval) {
@@ -90,21 +88,26 @@ class Time {
                 update_time_of_day(); // Обновляем время суток после изменения времени
             }
         }
-        void update_time_of_day() {
+
+        
+void Time::update_time_of_day() {
               if (hours >= 6 && hours < 18) {
                  time_of_day = 1;
               } else {
                 time_of_day = 0;
             }
         }   
-};
 
-
-
+Time::Time(int h, int m, int s) : hours(h), minutes(m), seconds(s), time_of_day(0), previousMillis(0) {
+            if (hours < 0 || hours > 23) hours = 0;
+            if (minutes < 0 || minutes > 59) minutes = 0;
+            if (seconds < 0 || seconds > 59) seconds = 0;
+            update_time_of_day(); 
+        };
 
 // Датчик освещенности
-int Illumination_sensor(Climate &climate){
-  if(climate.illumination_min < 30)
+int is_dark(Climate &climate){
+  if(analogRead(ILLUMINATION_PIN)< climate.illumination_min)
   return 1;
   else return 0;
 }
@@ -117,8 +120,8 @@ int Soil_humidity_sensor(){
 
 
 // Помпа
-void Pomp_switch(const boolean pomp_flag){
-  digitalWrite(POMP_PIN, pomp_flag);
+void PUMP_switch(const boolean PUMP_flag){
+  digitalWrite(PUMP_PIN, PUMP_flag);
 }
 
 
@@ -142,21 +145,21 @@ void Lamp_switch(const boolean lamp_flag){
 
  void Illumination_control(Time &mytime,Climate &climate){
   if (mytime.time_of_day)
-  Lamp_switch(Illumination_sensor(climate));
+  Lamp_switch(is_dark(climate));
 }
 
 
 void Humidity_soil_control(Climate &climate){
   static unsigned long int Hum_Time = 0;
-  if (Soil_humidity_sensor() > climate.humidity_soil_min and FLAG_POMP_SWITCH == false) 
+  if (Soil_humidity_sensor() > climate.humidity_soil_min && FLAG_PUMP_SWITCH == false && millis() - Hum_Time > 3 * climate.watering_interval) 
   {
-    Pomp_switch(true);
-    FLAG_POMP_SWITCH = true;
+    PUMP_switch(true);
+    FLAG_PUMP_SWITCH = true;
     Hum_Time = millis(); 
   }
  else if (millis() - Hum_Time > climate.watering_interval){
-    Pomp_switch(false);
-    FLAG_POMP_SWITCH = false;
+    PUMP_switch(false);
+    FLAG_PUMP_SWITCH = false; 
  }
 }
 
@@ -164,27 +167,38 @@ void Humidity_soil_control(Climate &climate){
 void temperature_control(Climate &climate){
   float temp;
   temp = dht.readTemperature(); 
-  if (temp < climate.temperature_min  and FLAG_POMP_SWITCH == false){ 
+  if (temp < climate.temperature_min  and FLAG_PUMP_SWITCH == false){ 
     Heating_switch(true);
+    FLAG_NAGREV_ON = true;
     Ventilation_switch(true);
   }
     else {
       Ventilation_switch(false);
       Heating_switch(false);
+      FLAG_NAGREV_ON = false;
     }
   }
   
 
   void humidity_control(Climate &climate) { 
     float hum = dht.readHumidity();
-    if (hum > climate.humidity_soil_min) {
+    if (hum > climate.humidity_soil_min ) {
       Ventilation_switch(true);
     }
-    else Ventilation_switch(false);
+    else if(FLAG_NAGREV_ON == false) Ventilation_switch(false);
 }
  
-
+void Ventilation_at_time(int hours) { 
+   static int last_hour = 0;
+    if (hours != last_hour) {
+      Ventilation_switch(true);
+    }
+    else if(FLAG_NAGREV_ON == false) Ventilation_switch(false);
+    last_hour = hours;
+}
+ 
   void Illumination_control(Time &mytime,Climate &climate);
+  void Ventilation_at_time(Time &Mytime);
   Time Mytime(14, 30, 0);
 void setup() {
   Serial.begin(9600);  
@@ -193,9 +207,7 @@ void setup() {
   pinMode(LAMP_PIN, OUTPUT);
   pinMode(FAN_PIN, OUTPUT);
   pinMode(NAGREV_PIN, OUTPUT);
-  pinMode(POMP_PIN, OUTPUT);
-  
- 
+  pinMode(PUMP_PIN, OUTPUT);
 
 }
 
@@ -207,4 +219,6 @@ void loop() {
   Humidity_soil_control(climate_tomato);
   temperature_control(climate_tomato);
   humidity_control(climate_tomato);
+  Ventilation_at_time(Mytime.hours); 
+  delay(10);
 }
