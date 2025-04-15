c365aa92d0caf8c88684cbea82cac6d360ef3c2d
im.makarov <makilya04@gmail.com>
2025-04-15 12:07:57 +0300

    ADD gh

---
+++ MakarovIM/task3/gh.ino
@@ -0,0 +1,186 @@
+#include "DHT.h"
+
+#define PIN_MOISTURE_SENSOR A1    
+#define PIN_LIGHT_SENSOR A0     
+#define PIN_DHT 8              
+#define PIN_PUMP 5             
+#define PIN_LIGHT 6       
+#define PIN_FAN 7              
+#define PIN_HEATER 4
+
+const int MIN_TEMP = 19;
+const int NORMAL_TEMP = 26;
+const int MAX_HUMIDITY = 90;
+const int NORMAL_HUMIDITY = 60;
+const int MIN_MOISTURE = 40;
+const int NORMAL_MOISTURE = 60;
+const int MIN_LIGHT = 30;
+
+DHT dht(PIN_DHT, DHT11);
+unsigned long start_time = 0;
+const unsigned long DAY_TIME = 86400;
+const unsigned long WEEK_TIME = DAY_TIME * 7;
+
+bool is_dark = false;
+bool is_cold = false;
+bool is_hot = false;
+bool is_soil_dry = false;
+bool is_soil_wet = false;
+bool is_air_wet = false;
+
+unsigned long light_activation_time = 0;
+unsigned long heater_activation_time = 0;
+unsigned long pump_activation_time = 0;
+unsigned long fan_activation_time = 0;
+
+float temperature = 0;
+float humidity = 0;
+int moisture = 0;
+int light = 0;
+
+void print_status()
+{
+  int current_day = millis() / DAY_TIME;
+  Serial.print("Параметры теплицы\n");
+  Serial.print("День с начала работы: ");
+  Serial.print(current_day);
+  Serial.print("\nВлажность воздуха: ");
+  Serial.print(humidity);
+  Serial.print("\nТемпература воздуха: ");
+  Serial.print(temperature);
+  Serial.print("\nВлажность почвы: ");
+  Serial.print(moisture);
+}
+
+
+void read_humidity()
+{
+  humidity = dht.readHumidity();
+
+  if (humidity > MAX_HUMIDITY) {
+    is_air_wet = true;
+  } else if (humidity <= NORMAL_HUMIDITY) {
+    is_air_wet = false;
+  }
+}
+
+
+void read_temperature()
+{
+  temperature = dht.readTemperature();
+  
+  if (temperature <= MIN_TEMP) {
+    is_cold = true;
+  } else if (temperature >= NORMAL_TEMP) {
+    is_cold = false;
+  }
+}
+
+
+void read_moisture()
+{
+  moisture = map(analogRead(PIN_MOISTURE_SENSOR), 1023, 0, 0, 100);
+
+  if (moisture < MIN_MOISTURE) {
+    is_soil_dry = true;
+  } else if (moisture >= NORMAL_MOISTURE) {
+    is_soil_dry = false;
+  }
+}
+
+
+void read_light()
+{
+  light = map(analogRead(PIN_LIGHT_SENSOR), 1023, 0, 0, 100);
+
+  if (light < MIN_LIGHT) {
+    is_dark = true;
+  } else {
+    is_dark = false;
+  }
+}
+
+
+void read_sensors()
+{
+  read_light();
+  read_moisture();
+  read_temperature();
+  read_humidity();
+}
+
+
+void manage_light()
+{
+  if (millis() - light_activation_time > 1000) {
+    if (is_dark) {
+      digitalWrite(PIN_LIGHT, HIGH);
+      light_activation_time = millis();
+    } else {
+      digitalWrite(PIN_LIGHT, LOW);
+    }
+  }
+}
+
+
+void manage_pump()
+{
+  if (millis() - pump_activation_time > 3000) {
+    if (is_soil_dry && millis() - pump_activation_time > 8000) {
+      digitalWrite(PIN_PUMP, HIGH);
+      pump_activation_time = millis();
+    } else {
+      digitalWrite(PIN_PUMP, LOW);
+    }
+  }
+}
+
+
+void manage_fan()
+{
+  if (is_cold || is_air_wet) {
+    digitalWrite(PIN_FAN, HIGH);
+  } else {
+    digitalWrite(PIN_FAN, LOW);
+  }
+}
+
+
+void manage_heater()
+{
+  if (is_cold) {
+    digitalWrite(PIN_HEATER, HIGH);
+  } else {
+    digitalWrite(PIN_HEATER, LOW);
+  }
+}
+
+
+void setup() 
+{
+  Serial.begin(9600);
+
+  pinMode(PIN_MOISTURE_SENSOR, INPUT);
+  pinMode(PIN_LIGHT_SENSOR, INPUT);
+  pinMode(PIN_DHT, INPUT);
+  pinMode(PIN_PUMP, OUTPUT);
+  pinMode(PIN_LIGHT, OUTPUT);
+  pinMode(PIN_FAN, OUTPUT);
+  pinMode(PIN_FAN, OUTPUT);
+  pinMode(PIN_HEATER, OUTPUT);
+
+  digitalWrite(PIN_PUMP, LOW);
+  digitalWrite(PIN_LIGHT, LOW);
+  digitalWrite(PIN_FAN, LOW);
+  digitalWrite(PIN_HEATER, LOW);
+}
+
+void loop() 
+{
+    read_sensors();
+    manage_light();
+    manage_fan();
+    manage_heater();
+    manage_pump();
+    print_status(); 
+}
--- SharyginAV/Matrix_cpp/matrix.cpp
+++ SharyginAV/Matrix_cpp/matrix.cpp
@@ -1,434 +1,140 @@
-#include "matrix.hpp"
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <cstdarg>
 
+using namespace std;
 
-Matrix::LogLevel Matrix::s_current_level = Matrix::LOG_INFO;
-
-MatrixException::MatrixException(const enum Exceptions code) : m_code(code) {}
-
-const char* MatrixException::what() const noexcept
+class Matrix
 {
-    switch(m_code)
-    {
-        case PARAMS_ERR:
-            return "Input parametrs error";
-        case SIZE_ERR:
-            return "Matrix wrong size";
-        default:
-            return nullptr;
-    }
-}
+public:
+    Matrix(size_t rows, size_t cols) : rows(rows), cols(cols), data(rows * cols, 0.0) {}
 
-enum MatrixException::Exceptions MatrixException::get_code() const noexcept
-{
-    return m_code;
-}
+    Matrix() : rows(0), cols(0) {}
 
-void Matrix::set_log_level(LogLevel level) noexcept
-{
-    s_current_level = level;
-}
+    Matrix(const Matrix &M) : rows(M.rows), cols(M.cols), data(M.data) {}  // конструктор копирования
 
+    ~Matrix() = default;
 
-void Matrix::print_log(LogLevel level, const char* format, ...) const noexcept
-{
-    if(level <= s_current_level) {
-        switch(level)
-        {
-        case LOG_ERR:
-            printf("\033[0;31mERROR: \033[0m");
-            break;
-        case LOG_WARN:
-            printf("\033[1;33mWARNING: \033[0m");
-            break;
-        case LOG_INFO:
-            printf("INFO: ");
-        default:
-            break;
+    double& operator()(const size_t row_num,const size_t col_num) {
+        if(row_num >= rows || col_num >= cols) {
+            throw "Out of range";
         }
-        va_list list;
-        va_start(list, format);
-        vprintf(format, list);
-        va_end(list);
-    }
-}
-
-
-Matrix::Matrix(size_t rows, size_t cols) : rows(rows), cols(cols), data(rows * cols, 0.0)
-{
-    print_log(LOG_INFO, "create matrix %ld x %ld\n", rows, cols);
-}
-
-
-Matrix::Matrix() : rows(0), cols(0)
-{
-    print_log(LOG_INFO, "create empty matrix\n");
-}
-
-
-Matrix::Matrix(const Matrix &M) : rows(M.rows), cols(M.cols), data(M.data)
-{
-    print_log(LOG_INFO, "copy matrix\n");
-}
-
-
-Matrix::Matrix(Matrix&& other) noexcept
-    : rows(other.rows), cols(other.cols), data(std::move(other.data)) {
-        other.rows = 0;
-        other.cols = 0;
-        print_log(LOG_INFO, "move matrix\n");
+        return data[row_num * cols + col_num];
     }
 
-
-Matrix::~Matrix()
-{
-    print_log(LOG_INFO, "matrix delete\n");
-}
-
-
-double& Matrix::operator()(const size_t row_num,const size_t col_num) {
-    if(row_num >= rows || col_num >= cols) {
-        print_log(LOG_ERR, "index out of range\n");
-        throw MatrixException(MatrixException::PARAMS_ERR);
-    }
-    return data[row_num * cols + col_num];
-}
-
-
-const double& Matrix::operator()(const size_t row_num, const size_t col_num) const {
-    if(row_num >= rows || col_num >= cols) {
-        print_log(LOG_ERR, "index out of range\n");
-        throw MatrixException(MatrixException::PARAMS_ERR);
-    }
-    return data[row_num * cols + col_num];
-}
-
-
-Matrix Matrix::operator+(const Matrix &A) const
-{
-    if(!A.equal_size(*this)) {
-        print_log(LOG_ERR, "matrices is not equal\n");
-        throw MatrixException(MatrixException::SIZE_ERR);
+    const double& operator()(const size_t row_num, const size_t col_num) const {
+        if(row_num >= rows || col_num >= cols) {
+            throw "Out of range";
+        }
+        return data[row_num * cols + col_num];
     }
 
-    Matrix result(A.rows, A.cols);
-    for(size_t idx = 0; idx < rows * cols; ++idx) {
-        result.data[idx] = data[idx] + A.data[idx];
+    Matrix operator+(const Matrix &A) const
+    {
+        if(!A.equal_size(*this)) {
+            throw "Matrices of unequal sizes";
+        }
+
+        Matrix result(A.rows, A.cols);
+        for(size_t idx = 0; idx < rows * cols; ++idx) {
+            result.data[idx] = data[idx] + A.data[idx];
+        }
+
+        return result;
     }
 
-    return result;
-}
+    Matrix operator-(const Matrix &A) const
+    {
+        if(!A.equal_size(*this)) {
+            throw "Matrices of unequal sizes";
+        }
 
+        Matrix result(A.rows, A.cols);
+        for(size_t idx = 0; idx < rows * cols; ++idx) {
+            result.data[idx] = data[idx] - A.data[idx];
+        }
 
-Matrix Matrix::operator-(const Matrix &A) const
-{
-    if(!A.equal_size(*this)) {
-        print_log(LOG_ERR, "matrices is not equal\n");
-        throw MatrixException(MatrixException::SIZE_ERR);
+        return result;
     }
 
-    Matrix result(A.rows, A.cols);
-    for(size_t idx = 0; idx < rows * cols; ++idx) {
-        result.data[idx] = data[idx] - A.data[idx];
-    }
+    Matrix& operator+=(const Matrix &A)
+    {
+        if(!A.equal_size(*this)) {
+            throw "Matrices of unequal sizes";
+        }
 
-    return result;
-}
+        for(size_t idx = 0; idx < rows * cols; ++idx) {
+            data[idx] += A.data[idx];
+        }
 
-
-Matrix& Matrix::operator+=(const Matrix &A)
-{
-    if(!A.equal_size(*this)) {
-        print_log(LOG_ERR, "matrices is not equal\n");
-        throw MatrixException(MatrixException::SIZE_ERR);
-    }
-
-    for(size_t idx = 0; idx < rows * cols; ++idx) {
-        data[idx] += A.data[idx];
-    }
-
-    return *this;
-}
-
-
-Matrix& Matrix::operator-=(const Matrix &A)
-{
-    if(!A.equal_size(*this)) {
-        print_log(LOG_ERR, "matrices is not equal\n");
-        throw MatrixException(MatrixException::SIZE_ERR);
-    }
-
-    for(size_t idx = 0; idx < rows * cols; ++idx) {
-        data[idx] -= A.data[idx];
-    }
-
-    return *this;
-}
-
-
-Matrix& Matrix::operator=(Matrix&& other) noexcept {
-    if(this == &other) {
-        print_log(LOG_INFO, "move matrix\n");
-        return *this; 
-    }
-    rows = other.rows;
-    cols = other.cols;
-    data = std::move(other.data);
-    other.rows = 0;
-    other.cols = 0;
-    print_log(LOG_INFO, "move matrix\n");
-    return *this;
-}
-
-
-Matrix& Matrix::operator=(const Matrix &A)
-{
-    if(data == A.data) {
         return *this;
     }
 
-    rows = A.rows;
-    cols = A.cols;
-    data = A.data;
+    Matrix& operator-=(const Matrix &A)
+    {
+        if(!A.equal_size(*this)) {
+            throw "Matrices of unequal sizes";
+        }
 
-    return *this;
-}
+        for(size_t idx = 0; idx < rows * cols; ++idx) {
+            data[idx] -= A.data[idx];
+        }
 
-
-Matrix Matrix::operator*(const double num)
-{
-    Matrix res(*this);
-
-    for(size_t idx = 0; idx < rows * cols; ++idx) {
-        res.data[idx] *= num;
+        return *this;
     }
 
-    return res;
-}
-
-
-Matrix Matrix::operator*(const Matrix &A)
-{
-    if(cols != A.rows) {
-        print_log(LOG_ERR, "number of columns of first matrix is ​​not equal to number of rows of second matrix\n");
-        throw MatrixException(MatrixException::SIZE_ERR);
-    }
-    Matrix res(rows, A.cols);
-
-    for(size_t row_idx = 0; row_idx < rows; ++row_idx) {
-        for(size_t col_idx = 0; col_idx < A.cols; ++col_idx) {
-            for(size_t idx = 0; idx < A.rows; ++idx) {
-                res(row_idx, col_idx) += (*this)(row_idx, idx) * A(idx, col_idx);
+    void print() const noexcept
+    {
+        for(size_t row_idx = 0; row_idx < rows; ++row_idx) {
+            for(size_t col_idx = 0; col_idx < cols; ++col_idx) {
+                std::cout << (*this)(row_idx, col_idx);
+                col_idx == cols - 1 ? std::cout << '\n' : std::cout << '\t';
             }
         }
     }
 
-    return res;
-}
-
-
-Matrix& Matrix::operator*=(const double num)
-{
-    for(size_t idx = 0; idx < rows * cols; ++idx) {
-        data[idx] *= num;
+    bool equal_size(const Matrix &A) const
+    {
+        return (rows == A.rows) && (cols == A.cols);
     }
 
-    return *this;
-}
+    Matrix transposition()
+    {
+        Matrix res(cols, rows);
 
-
-Matrix& Matrix::operator*=(const Matrix &A)
-{
-    if(cols != A.rows) {
-        print_log(LOG_ERR, "number of columns of first matrix is ​​not equal to number of rows of second matrix\n");
-        throw MatrixException(MatrixException::SIZE_ERR);
-    }
-
-    *this = (*this) * A;
-
-    return *this;
-}
-
-
-void Matrix::print() const noexcept
-{
-    for(size_t row_idx = 0; row_idx < rows; ++row_idx) {
-        for(size_t col_idx = 0; col_idx < cols; ++col_idx) {
-            std::cout << (*this)(row_idx, col_idx);
-            col_idx == cols - 1 ? std::cout << '\n' : std::cout << '\t';
+        for(size_t row_idx = 0; row_idx < rows; ++row_idx) {
+            for(size_t col_idx = 0; col_idx < cols; ++col_idx) {
+                res(col_idx, row_idx) = (*this)(row_idx, col_idx);
+            }            
         }
-    }
-}
 
-
-bool Matrix::equal_size(const Matrix &A) const
-{
-    return (rows == A.rows) && (cols == A.cols);
-}
-
-
-bool Matrix::is_square() const
-{
-    return rows == cols;
-}
-
-
-void Matrix::set_identity()
-{
-    if(!(*this).is_square()) {
-        print_log(LOG_ERR, "matrix is not square\n");
-        throw MatrixException(MatrixException::SIZE_ERR);
+        return res;
     }
 
-    data.assign(rows * cols, 0.0);
-
-    for(size_t idx = 0; idx < rows; ++idx) {
-        (*this)(idx, idx) = 1;
-    }
-}
-
-
-Matrix Matrix::transpos()
-{
-    Matrix res(cols, rows);
-
-    for(size_t row_idx = 0; row_idx < rows; ++row_idx) {
-        for(size_t col_idx = 0; col_idx < cols; ++col_idx) {
-            res(col_idx, row_idx) = (*this)(row_idx, col_idx);
-        }            
+    void get_size()
+    {
+        cout << "Количество строк: " << rows << "\tКоличество столбцов: " << cols << '\n';
     }
 
-    return res;
-}
-
-
-Matrix Matrix::pow(const unsigned int pow) const
-{
-    if(!(*this).is_square()) {
-        print_log(LOG_ERR, "matrix is not square\n");
-        throw MatrixException(MatrixException::SIZE_ERR);
-    }
     
-    Matrix res(rows, cols);
-    res.set_identity();
 
-    for(unsigned int idx = 0; idx < pow; ++idx) {
-        res *= (*this);
-    }
-
-    return res;
-}
+private:
+    size_t rows;
+    size_t cols;
+    std::vector<double> data;
+};
 
 
-Matrix Matrix::exp(const unsigned int iterations) const
+int main()
 {
-    if(!this->is_square()) {
-        print_log(LOG_ERR, "matrix is not square\n");
-        throw MatrixException(MatrixException::SIZE_ERR);
-    }
-
-    Matrix res(rows, cols);
-    res.set_identity();
-
-    Matrix term(rows, cols);
-    term.set_identity();
-
-    for(unsigned int idx = 1; idx < iterations; ++idx) {
-        term *= (*this);
-        term *= (1 / (double)idx);
-        res += term;
-    }
-
-    return res;
+    Matrix m1(2, 3);
+    m1(0, 0) = 1;
+    m1(0, 1) = 2;
+    m1(1, 0) = 3;
+    m1(1, 1) = 4;
+    
+    m1.print();
+    m1.get_size();
+    m1 = m1.transposition();
+    m1.print();
+    m1.get_size();
 }
-
-
-void Matrix::get_size()
-{
-    std::cout << "Количество строк: " << rows << "\tКоличество столбцов: " << cols << '\n';
-}
-
-
-size_t Matrix::find_non_zero_in_col(const size_t idx_start) const
-{   
-    if(idx_start >= rows) {
-        print_log(LOG_ERR, "out of range\n");
-        throw MatrixException(MatrixException::PARAMS_ERR);
-    }
-
-    for(size_t row_idx = idx_start + 1; row_idx < rows; ++row_idx) {
-        if(fabs((*this)(row_idx, row_idx)) >= 0.00001) {
-            return row_idx;
-        }
-    }
-
-    return 0;
-}
-
-void Matrix::swap_rows(const size_t row_1, const size_t row_2)
-{
-    if(row_1 >= rows || row_2 >= rows) {
-        print_log(LOG_ERR, "out of range\n");
-        throw MatrixException(MatrixException::PARAMS_ERR);
-    }
-
-    double tmp = 0.0;
-
-    for(size_t col_idx = 0; col_idx < cols; ++col_idx) {
-        tmp = (*this)(row_1, col_idx);
-        (*this)(row_1, col_idx) = (*this)(row_2, col_idx);
-        (*this)(row_2, col_idx) = tmp;
-    }
-}
-
-
-void Matrix::sub_row(const size_t row, const size_t row_base, const double ratio)
-{
-    if(row >= rows || row_base >= rows) {
-        print_log(LOG_ERR, "out of range\n");
-        throw MatrixException(MatrixException::PARAMS_ERR);
-    }
-
-    for(size_t col_idx = 0; col_idx < cols; ++col_idx) {
-        (*this)(row, col_idx) -= ratio * (*this)(row_base, col_idx);
-    }
-}
-
-
-double Matrix::det() const
-{
-    if(!this->is_square()) {
-        print_log(LOG_ERR, "Matrix is not square\n");
-        throw MatrixException(MatrixException::SIZE_ERR);
-    }
-
-    Matrix tmp(*this);
-
-    double det = 1.0;
-    for(size_t row_idx = 0; row_idx < rows - 1; ++row_idx) {
-        if(fabs(tmp(row_idx, row_idx)) < 0.00001) {
-            size_t idx_non_zero = tmp.find_non_zero_in_col(row_idx);
-            if(idx_non_zero == 0) {
-                return 0.0;
-            }
-
-            tmp.swap_rows(row_idx, idx_non_zero);
-            det *= -1.0;
-        }
-
-        for(size_t row = row_idx + 1; row < rows; ++row) {
-            double mult = tmp(row, row_idx) / tmp(row_idx, row_idx);
-            tmp.sub_row(row, row_idx, mult);
-        }
-    }
-
-    for(size_t idx = 0; idx < rows; ++idx) {
-        det *= tmp(idx, idx);
-    }
-
-    return det;
-}
-
