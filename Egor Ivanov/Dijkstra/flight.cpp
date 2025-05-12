124152b17a9a849ece2eb30724296c696e716fc6
Egor12567 <ivanov.egor.01072004@mail.ru>
2025-05-12 23:13:12 +0300

    Add files via upload

---
+++ Egor Ivanov/Dijkstra/flight.cpp
@@ -0,0 +1,234 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <limits>
+
+
+class Vertex {
+public:
+    int prev_vert = 0;
+    int time_from_start = std::numeric_limits<int>::max();
+};
+
+
+class Neighbor {
+public:
+    int vertex;
+    int time;
+};
+
+
+struct MinHeap {
+private:
+    std::vector<Neighbor> heap;
+public:
+    Neighbor& operator[](size_t index)
+    {
+        return heap[index];
+    }
+
+    size_t size() const
+    {
+        return heap.size();
+    }
+
+    bool empty() const
+    {
+        return heap.empty();
+    }
+
+    void heapify_down(int idx) 
+    {
+        int left = 2 * idx + 1;
+        int right = 2 * idx + 2;
+        int smallest = idx;
+
+        if (left < heap.size() && heap[left].time < heap[smallest].time)
+        {
+            smallest = left;
+        }
+        if (right < heap.size() && heap[right].time < heap[smallest].time)
+        {
+            smallest = right;
+        }
+
+        if (smallest != idx)
+        {
+            std::swap(heap[idx], heap[smallest]);
+            heapify_down(smallest);
+        }
+    }
+
+    void heapify_up(size_t idx)
+    {
+        size_t parent = (idx - 1) / 2;
+        while (idx > 0 && heap[idx].time < heap[parent].time)
+        {
+            std::swap(heap[idx], heap[parent]);
+            idx = parent;
+            parent = (idx - 1) / 2;
+        }
+    }
+
+    void push(const Neighbor& neighbor) 
+    {
+        heap.push_back(neighbor);
+        heapify_up(heap.size() - 1);
+    }
+
+    Neighbor pop()
+    {
+        if (heap.empty())
+        {
+            throw std::out_of_range("Heap is empty");
+        }
+
+        Neighbor min = heap[0];
+        heap[0] = heap.back();
+        heap.pop_back();
+        heapify_down(0);
+
+        return min;
+    }
+};
+
+
+void dijkstra(int start, int end, const std::vector<std::vector<int>>& fl_table)
+{
+    size_t table_size = fl_table.size();
+    std::vector<Vertex> Verts(table_size);
+    std::vector<int> stack_to_go;
+    std::vector<bool> in_heap(table_size, false);
+    MinHeap heap;
+
+    heap.push({ start, 0 });
+    Verts[start].time_from_start = 0;
+
+    while (!heap.empty())
+    {
+        int current_vertex = heap.pop().vertex;
+
+        for (size_t neighbor = 0; neighbor < table_size; ++neighbor)
+        {
+            if (fl_table[current_vertex][neighbor] != std::numeric_limits<int>::max())
+            {
+                stack_to_go.push_back(neighbor);
+            }
+        }
+
+        for (int neighbor : stack_to_go)
+        {
+            int new_time = Verts[current_vertex].time_from_start + fl_table[current_vertex][neighbor];
+            if (new_time < Verts[neighbor].time_from_start)
+            {
+                Verts[neighbor].time_from_start = new_time;
+                Verts[neighbor].prev_vert = current_vertex;
+                if (!in_heap[neighbor])
+                {
+                    heap.push({ neighbor, new_time });
+                    in_heap[neighbor] = true;
+                }
+                else
+                {
+                    for (size_t idx = 0; idx < heap.size(); ++idx)
+                    {
+                        if (heap[idx].vertex == neighbor)
+                        {
+                            heap[idx].time = new_time;
+                            heap.heapify_up(idx);
+                            heap.heapify_down(idx);
+                        }
+                    }
+                }
+            }
+        }
+        stack_to_go.clear();
+    }
+
+    if (Verts[end].time_from_start == std::numeric_limits<int>::max()) // вывод результата 
+    {
+        std::cout << "No path found" << std::endl;
+    }
+    else
+    {
+        std::cout << "Shortest path: ";
+
+        for (int idx_vert = end; idx_vert != start; idx_vert = Verts[idx_vert].prev_vert)
+        {
+            std::cout << idx_vert << " <- ";
+        }
+        std::cout << start << std::endl;
+        std::cout << "Time: " << Verts[end].time_from_start << std::endl;
+    }
+}
+
+
+void filling_table(std::vector<std::vector<int>>& flight_table)
+{
+    std::ifstream datafile;
+    datafile.open("logfile.txt");
+
+    if (!datafile.is_open())
+    {
+        std::cout << "Couldn't open datafile" << std::endl;
+        return;
+    }
+
+    std::string txtline;
+    while (!datafile.eof())
+    {
+        getline(datafile, txtline);
+        std::stringstream inputString(txtline);
+
+        int str, col, time;
+        std::string tempString;
+
+        getline(inputString, tempString, '\t');
+        str = atoi(tempString.c_str());
+
+        getline(inputString, tempString, '\t');
+        col = atoi(tempString.c_str());
+
+        getline(inputString, tempString, '\t');
+        time = atoi(tempString.c_str());
+
+        if (flight_table[str][col] > time)
+        {
+            flight_table[str][col] = time;
+        }
+    }
+}
+
+
+int main() {
+    int table_size = 40 * 1000;
+    int start, end;
+    unsigned int state, run = 1;
+
+    std::vector<std::vector<int>> flight_table(table_size, std::vector<int>(table_size, std::numeric_limits<int>::max()));
+
+    filling_table(flight_table);
+
+    while (run)
+    {
+        std::cout << "To start press 1: ";
+        std::cin >> state;
+
+        switch (state)
+        {
+        case 0:
+            run = 0;
+            break;
+        case 1:
+            std::cout << "Start: ";
+            std::cin >> start;
+            std::cout << "End: ";
+            std::cin >> end;
+            dijkstra(start, end, flight_table);
+            break;
+        }
+    }
+
+    return 0;
+}
---
+++ Egor Ivanov/Dijkstra/parsing.cpp
@@ -0,0 +1,127 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+
+class Flight {
+public:
+    double time;
+    double carrier_id;
+    std::string carrier_name;
+    double city_from_id;
+    std::string city_from;
+    double city_to_id;
+    std::string city_to;
+
+    Flight(double air_time, double airline_id, std::string origin_carrier_name,
+        double origin_city_id, std::string origin_city, double destination_city_id,
+        std::string destination_city);
+
+};
+
+
+Flight::Flight(double air_time, double airline_id, std::string origin_carrier_name,
+    double origin_city_id, std::string origin_city, double destination_city_id,
+    std::string destination_city)
+{
+    time = air_time;
+    carrier_id = airline_id;
+    carrier_name = origin_carrier_name;
+    city_from_id = origin_city_id;
+    city_from = origin_city;
+    city_to_id = destination_city_id;
+    city_to = destination_city;
+}
+
+
+void Save_to_file(std::ofstream& logfile, const std::vector<Flight>& Flights)
+{
+    logfile << "fr.id" << "\t" << "to.id" << "\t" << "time" << "\t"
+        << "car.id" << "\t" << "car.name" << "\t" << "ci.fr." << "\t"
+        << "ci.to" << std::endl;
+
+    for (const auto& flight : Flights)
+    {
+        logfile << flight.city_from_id << "\t" << flight.city_to_id << "\t" << flight.time << "\t"
+            << flight.carrier_id << "\t" << flight.carrier_name << "\t" << flight.city_from << "\t"
+            << flight.city_to << std::endl;
+    }
+}
+
+
+int main()
+{
+    std::ifstream datafile;
+    datafile.open("T_T100_SEGMENT_ALL_CARRIER.csv");
+
+    if (!datafile.is_open())
+    {
+        std::cout << "Unable to open data file1\n";
+        return 0;
+    }
+
+    std::ofstream logfile;
+    logfile.open("logfile.txt");
+
+    if (!logfile.is_open())
+    {
+        std::cout << "Unable to open data file2\n";
+        return 0;
+    }
+
+    std::vector<Flight> Flights;
+    std::string csvline;
+
+    while (getline(datafile, csvline))
+    {
+        std::stringstream inputString(csvline);
+
+        std::string tempString;
+        int departures_performed;
+        std::string carrier_name;
+        std::string city_from;
+        std::string city_to;
+        double time, carrier_id, city_from_id, city_to_id;
+
+        size_t add = 0;
+
+        for (size_t idx = 0; idx < 2; ++idx) { getline(inputString, tempString, ','); }
+        departures_performed = atoi(tempString.c_str());
+
+        for (size_t idx = 0; idx < 8; ++idx) { getline(inputString, tempString, ','); }
+        time = atof(tempString.c_str());
+
+        for (size_t idx = 0; idx < 2; ++idx) { getline(inputString, tempString, ','); }
+        carrier_id = atof(tempString.c_str());
+
+        getline(inputString, carrier_name, ',');
+        if (carrier_name[0] == '\"')
+        {
+            carrier_name.erase(0, 1);
+            add = 2;
+        }
+
+        for (size_t idx = 0; idx < 9 + add; ++idx) { getline(inputString, tempString, ','); }
+        city_from_id = atof(tempString.c_str());
+        getline(inputString, city_from, ',');
+
+        for (size_t idx = 0; idx < 11; ++idx) { getline(inputString, tempString, ','); }
+        city_to_id = atof(tempString.c_str());
+        getline(inputString, city_to, ',');
+
+
+        if (time > 0 && departures_performed != 0)
+        {
+            Flight flight(time, carrier_id, carrier_name, city_from_id, city_from, city_to_id, city_to);
+            Flights.push_back(flight);
+        }
+    }
+
+    Save_to_file(logfile, Flights);
+
+    datafile.close();
+    logfile.close();
+    return 0;
+}
