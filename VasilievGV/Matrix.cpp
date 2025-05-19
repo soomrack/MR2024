c857bb4d2f41d09c339f139d370af01e363de50b
Tuc-Bambook <grigoriy1207@gmail.com>
2025-05-20 00:13:41 +0300

    Matrix.cpp

--- VasilievGV/Matrix.cpp
+++ VasilievGV/Matrix.cpp
@@ -2,8 +2,7 @@
 #include <stdexcept>
 #include <cstring>
 #include <cstdlib>
-#include <cstdint> 
-
+#include <cstdint>
 
 class MatrixError : public std::runtime_error {
 public:
@@ -14,234 +13,271 @@ MatrixError ERR_MEM("Allocation failed.");
 MatrixError ERR_DIM("Incompatible dimensions.");
 MatrixError ERR_DATA("Matrix buffer is null.");
 
-class Mat {
+class Matrix {
 private:
-    size_t h_, w_;
-    double* buffer_;
+    size_t rows_, cols_;
+    double* data_;
 
-    void check_dimensions(const Mat& other) const {
-        if (h_ != other.h_ || w_ != other.w_)
+    void ensure_same_shape(const Matrix& other) const {
+        if (rows_ != other.rows_ || cols_ != other.cols_)
             throw ERR_DIM;
     }
 
 public:
-    Mat();
-    Mat(size_t rows, size_t cols);
-    Mat(size_t rows, size_t cols, const double* source);
-    Mat(const Mat& other);
-    Mat(Mat&& other) noexcept;
-    ~Mat();
+    Matrix();
+    Matrix(size_t r, size_t c);
+    Matrix(size_t r, size_t c, const double* src);
+    Matrix(const Matrix& other);
+    Matrix(Matrix&& other) noexcept;
+    ~Matrix();
 
-    Mat& operator=(const Mat& rhs);
-    Mat& operator=(Mat&& rhs) noexcept;
+    Matrix& operator=(const Matrix& rhs);
+    Matrix& operator=(Matrix&& rhs) noexcept;
 
-    Mat operator+(const Mat& rhs) const;
-    Mat operator-(const Mat& rhs) const;
-    Mat operator*(const Mat& rhs) const;
-    Mat operator*(double scalar) const;
+    Matrix operator+(const Matrix& rhs) const;
+    Matrix operator-(const Matrix& rhs) const;
+    Matrix operator*(const Matrix& rhs) const;
+    Matrix operator*(double scalar) const;
 
-    Mat& operator+=(const Mat& rhs);
-    Mat& operator-=(const Mat& rhs);
-    Mat& operator*=(const Mat& rhs);
-    Mat& operator*=(double scalar);
+    Matrix& operator+=(const Matrix& rhs);
+    Matrix& operator-=(const Matrix& rhs);
+    Matrix& operator*=(const Matrix& rhs);
+    Matrix& operator*=(double scalar);
 
     void print() const;
-    void fillRandom();
-    void toZero();
-    void toIdentity();
+
+    void randomize();
+    void reset();
+    void identity();
+
     double determinant() const;
-    void raiseTo(int power);
-    Mat transpose() const;
-    Mat exp(unsigned long long terms) const;
+
+    void power(int exponent);
+    Matrix transpose() const;
+    Matrix exp(unsigned long long terms) const;
 };
 
-// --- Implementations ---
+Matrix::Matrix() : rows_(0), cols_(0), data_(nullptr) {}
 
-Mat::Mat() : h_(0), w_(0), buffer_(nullptr) {}
+Matrix::Matrix(size_t r, size_t c) : rows_(r), cols_(c), data_(nullptr) {
+    if (r == 0 || c == 0) throw ERR_DIM;
 
-Mat::Mat(size_t r, size_t c) : h_(r), w_(c), buffer_(nullptr) {
-    if (r == 0 || c == 0) return;
-    if (r > SIZE_MAX / sizeof(double) / c) throw ERR_MEM;
-    buffer_ = new double[r * c];
+    if (r > SIZE_MAX / sizeof(double) / c)
+        throw ERR_MEM;
+
+    data_ = new double[r * c];
 }
 
-Mat::Mat(size_t r, size_t c, const double* source) : h_(r), w_(c) {
-    if (!source) throw ERR_DATA;
-    buffer_ = new double[h_ * w_];
-    std::memcpy(buffer_, source, sizeof(double) * h_ * w_);
+Matrix::Matrix(size_t r, size_t c, const double* src) : rows_(r), cols_(c) {
+    if (!src) throw ERR_DATA;
+
+    data_ = new double[rows_ * cols_];
+    std::memcpy(data_, src, sizeof(double) * rows_ * cols_);
 }
 
-Mat::Mat(const Mat& other) : h_(other.h_), w_(other.w_) {
-    buffer_ = new double[h_ * w_];
-    std::memcpy(buffer_, other.buffer_, sizeof(double) * h_ * w_);
+Matrix::Matrix(const Matrix& other) : rows_(other.rows_), cols_(other.cols_) {
+    data_ = new double[rows_ * cols_];
+    std::memcpy(data_, other.data_, sizeof(double) * rows_ * cols_);
 }
 
-Mat::Mat(Mat&& other) noexcept : h_(other.h_), w_(other.w_), buffer_(other.buffer_) {
-    other.buffer_ = nullptr;
-    other.h_ = other.w_ = 0;
+Matrix::Matrix(Matrix&& other) noexcept : rows_(other.rows_), cols_(other.cols_), data_(other.data_) {
+    other.data_ = nullptr;
+    other.rows_ = other.cols_ = 0;
 }
 
-Mat::~Mat() {
-    delete[] buffer_;
+Matrix::~Matrix() {
+    delete[] data_;
 }
 
-Mat& Mat::operator=(const Mat& rhs) {
+Matrix& Matrix::operator=(const Matrix& rhs) {
     if (this == &rhs) return *this;
-    delete[] buffer_;
-    h_ = rhs.h_;
-    w_ = rhs.w_;
-    buffer_ = new double[h_ * w_];
-    std::memcpy(buffer_, rhs.buffer_, sizeof(double) * h_ * w_);
+
+    delete[] data_;
+
+    rows_ = rhs.rows_;
+    cols_ = rhs.cols_;
+
+    data_ = new double[rows_ * cols_];
+    std::memcpy(data_, rhs.data_, sizeof(double) * rows_ * cols_);
+
     return *this;
 }
 
-Mat& Mat::operator=(Mat&& rhs) noexcept {
+Matrix& Matrix::operator=(Matrix&& rhs) noexcept {
     if (this != &rhs) {
-        delete[] buffer_;
-        h_ = rhs.h_;
-        w_ = rhs.w_;
-        buffer_ = rhs.buffer_;
-        rhs.buffer_ = nullptr;
-        rhs.h_ = rhs.w_ = 0;
+        delete[] data_;
+
+        rows_ = rhs.rows_;
+        cols_ = rhs.cols_;
+        data_ = rhs.data_;
+
+        rhs.data_ = nullptr;
+        rhs.rows_ = rhs.cols_ = 0;
     }
     return *this;
 }
 
-void Mat::print() const {
-    for (size_t i = 0; i < h_; ++i) {
-        for (size_t j = 0; j < w_; ++j) {
-            std::cout << buffer_[i * w_ + j] << " ";
-        }
+void Matrix::print() const {
+    for (size_t i = 0; i < rows_; ++i) {
+        for (size_t j = 0; j < cols_; ++j)
+            std::cout << data_[i * cols_ + j] << " ";
         std::cout << "\n";
     }
 }
 
-void Mat::fillRandom() {
-    for (size_t i = 0; i < h_ * w_; ++i)
-        buffer_[i] = rand() % 10;
+void Matrix::randomize() {
+    for (size_t i = 0; i < rows_ * cols_; ++i)
+        data_[i] = rand() % 10;
 }
 
-void Mat::toZero() {
-    if (!buffer_) throw ERR_DATA;
-    std::memset(buffer_, 0, sizeof(double) * h_ * w_);
+void Matrix::reset() {
+    if (!data_) throw ERR_DATA;
+    std::memset(data_, 0, sizeof(double) * rows_ * cols_);
 }
 
-void Mat::toIdentity() {
-    if (h_ != w_) throw ERR_DIM;
-    toZero();
-    for (size_t i = 0; i < h_; ++i)
-        buffer_[i * w_ + i] = 1.0;
+void Matrix::identity() {
+    if (rows_ != cols_) throw ERR_DIM;
+
+    reset();
+
+    for (size_t i = 0; i < rows_; ++i)
+        data_[i * cols_ + i] = 1.0;
 }
 
-double Mat::determinant() const {
-    if (h_ != w_) throw ERR_DIM;
-    if (h_ == 1) return buffer_[0];
-    if (h_ == 2) return buffer_[0] * buffer_[3] - buffer_[1] * buffer_[2];
-    if (h_ == 3) {
-        return buffer_[0] * (buffer_[4] * buffer_[8] - buffer_[5] * buffer_[7])
-             - buffer_[1] * (buffer_[3] * buffer_[8] - buffer_[5] * buffer_[6])
-             + buffer_[2] * (buffer_[3] * buffer_[7] - buffer_[4] * buffer_[6]);
+double Matrix::determinant() const {
+    if (rows_ != cols_) throw ERR_DIM;
+
+    if (rows_ == 1)
+        return data_[0];
+
+    if (rows_ == 2)
+        return data_[0] * data_[3] - data_[1] * data_[2];
+
+    if (rows_ == 3) {
+        return data_[0] * (data_[4] * data_[8] - data_[5] * data_[7])
+             - data_[1] * (data_[3] * data_[8] - data_[5] * data_[6])
+             + data_[2] * (data_[3] * data_[7] - data_[4] * data_[6]);
     }
+
     throw ERR_DIM;
 }
 
-Mat Mat::transpose() const {
-    Mat res(w_, h_);
-    for (size_t i = 0; i < h_; ++i)
-        for (size_t j = 0; j < w_; ++j)
-            res.buffer_[j * h_ + i] = buffer_[i * w_ + j];
+Matrix Matrix::transpose() const {
+    Matrix res(cols_, rows_);
+
+    for (size_t i = 0; i < rows_; ++i)
+        for (size_t j = 0; j < cols_; ++j)
+            res.data_[j * rows_ + i] = data_[i * cols_ + j];
+
     return res;
 }
 
-Mat Mat::exp(unsigned long long terms) const {
-    if (h_ != w_) throw ERR_DIM;
-    Mat result(h_, w_);
-    result.toIdentity();
-    Mat term(*this);
+Matrix Matrix::exp(unsigned long long terms) const {
+    if (rows_ != cols_) throw ERR_DIM;
+
+    Matrix result(rows_, cols_);
+    result.identity();
+
+    Matrix term(*this);
+
     for (unsigned long long k = 1; k <= terms; ++k) {
         term = term * (*this) * (1.0 / k);
         result += term;
     }
+
     return result;
 }
 
-void Mat::raiseTo(int power) {
-    if (h_ != w_) throw ERR_DIM;
-    Mat res(h_, w_);
-    res.toIdentity();
-    for (int i = 0; i < power; ++i)
-        res *= *this;
-    *this = res;
+void Matrix::power(int exponent) {
+    if (rows_ != cols_) throw ERR_DIM;
+
+    Matrix result(rows_, cols_);
+    result.identity();
+
+    for (int i = 0; i < exponent; ++i)
+        result *= *this;
+
+    *this = result;
 }
 
-Mat& Mat::operator+=(const Mat& rhs) {
-    check_dimensions(rhs);
-    for (size_t i = 0; i < h_ * w_; ++i)
-        buffer_[i] += rhs.buffer_[i];
+Matrix& Matrix::operator+=(const Matrix& rhs) {
+    ensure_same_shape(rhs);
+
+    for (size_t i = 0; i < rows_ * cols_; ++i)
+        data_[i] += rhs.data_[i];
+
     return *this;
 }
 
-Mat& Mat::operator-=(const Mat& rhs) {
-    check_dimensions(rhs);
-    for (size_t i = 0; i < h_ * w_; ++i)
-        buffer_[i] -= rhs.buffer_[i];
+Matrix& Matrix::operator-=(const Matrix& rhs) {
+    ensure_same_shape(rhs);
+
+    for (size_t i = 0; i < rows_ * cols_; ++i)
+        data_[i] -= rhs.data_[i];
+
     return *this;
 }
 
-Mat& Mat::operator*=(const Mat& rhs) {
-    if (w_ != rhs.h_) throw ERR_DIM;
-    Mat result(h_, rhs.w_);
-    for (size_t i = 0; i < h_; ++i) {
-        for (size_t j = 0; j < rhs.w_; ++j) {
+Matrix& Matrix::operator*=(const Matrix& rhs) {
+    if (cols_ != rhs.rows_) throw ERR_DIM;
+
+    Matrix result(rows_, rhs.cols_);
+
+    for (size_t i = 0; i < rows_; ++i) {
+        for (size_t j = 0; j < rhs.cols_; ++j) {
             double sum = 0;
-            for (size_t k = 0; k < w_; ++k) {
-                sum += buffer_[i * w_ + k] * rhs.buffer_[k * rhs.w_ + j];
-            }
-            result.buffer_[i * rhs.w_ + j] = sum;
+
+            for (size_t k = 0; k < cols_; ++k)
+                sum += data_[i * cols_ + k] * rhs.data_[k * rhs.cols_ + j];
+
+            result.data_[i * rhs.cols_ + j] = sum;
         }
     }
+
     *this = result;
     return *this;
 }
 
-Mat& Mat::operator*=(double scalar) {
-    for (size_t i = 0; i < h_ * w_; ++i)
-        buffer_[i] *= scalar;
+Matrix& Matrix::operator*=(double scalar) {
+    for (size_t i = 0; i < rows_ * cols_; ++i)
+        data_[i] *= scalar;
     return *this;
 }
 
-Mat Mat::operator+(const Mat& rhs) const {
-    Mat result(*this);
+Matrix Matrix::operator+(const Matrix& rhs) const {
+    Matrix result(*this);
     result += rhs;
     return result;
 }
 
-Mat Mat::operator-(const Mat& rhs) const {
-    Mat result(*this);
+Matrix Matrix::operator-(const Matrix& rhs) const {
+    Matrix result(*this);
     result -= rhs;
     return result;
 }
 
-Mat Mat::operator*(const Mat& rhs) const {
-    Mat result(*this);
+Matrix Matrix::operator*(const Matrix& rhs) const {
+    Matrix result(*this);
     result *= rhs;
     return result;
 }
 
-Mat Mat::operator*(double scalar) const {
-    Mat result(*this);
+Matrix Matrix::operator*(double scalar) const {
+    Matrix result(*this);
     result *= scalar;
     return result;
 }
 
 int main() {
-    double values[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-    Mat mat(3, 3, values);
-    mat.print();
+    double vals[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    Matrix m(3, 3, vals);
 
-    std::cout << "\nAfter multiplying by 10:\n";
-    Mat scaled = mat * 10;
+    m.print();
+
+    std::cout << "\nScaled by 10:\n";
+    Matrix scaled = m * 10;
     scaled.print();
 
+    std::cout << "\nDeterminant: " << scaled.determinant() << "\n";
     return 0;
 }
